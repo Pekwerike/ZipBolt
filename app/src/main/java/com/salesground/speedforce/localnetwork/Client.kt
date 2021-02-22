8e03deb40787d5f300d0ccd8afdfb14d03b84011
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 16:33:14 +0100

    Completed Client class

--- app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
+++ app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
@@ -6,13 +6,23 @@ import kotlinx.coroutines.withContext
 import java.io.File
 import java.net.Socket
 
-class Client(private val serverIpAddress : String) {
+class Client(private val serverIpAddress: String) {
 
-   suspend fun connectToServer(parentFolder : File) {
-       withContext(Dispatchers.IO) {
-           val server: Socket = Socket(serverIpAddress, 8090)
+    private lateinit var fileTransferProtocol : FileTransferProtocol
 
-           FileTransferProtocol(server).receiveFile(parentFolder = parentFolder)
-       }
-   }
+    suspend fun connectToServer(filesToTransfer: MutableList<File>? = null, parentFolder: File? = null) {
+        withContext(Dispatchers.IO) {
+            val server: Socket = Socket(serverIpAddress, 8090)
+            fileTransferProtocol = FileTransferProtocol(server)
+
+            filesToTransfer?.let {
+                fileTransferProtocol.transferFile(it)
+            }
+            parentFolder?.let {
+                fileTransferProtocol.receiveFile(it)
+            }
+        }
+    }
+
+
 }
