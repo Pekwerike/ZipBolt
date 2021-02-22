8676faed8aca168a60828b0a890cc086725acb6d
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 16:23:10 +0100

    Connected FileTransferProtocol class to the Client and Receiver for various operations

--- app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
+++ app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
@@ -1,15 +1,18 @@
 package com.salesground.speedforce.localnetwork
 
+import com.salesground.speedforce.communicationprotocol.FileTransferProtocol
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.net.Socket
 
 class Client(private val serverIpAddress : String) {
 
-   suspend fun connectToServer() {
+   suspend fun connectToServer(parentFolder : File) {
        withContext(Dispatchers.IO) {
            val server: Socket = Socket(serverIpAddress, 8090)
 
+           FileTransferProtocol(server).receiveFile(parentFolder = parentFolder)
        }
    }
 }
--- app/src/main/java/com/salesground/speedforce/localnetwork/Server.kt
+++ app/src/main/java/com/salesground/speedforce/localnetwork/Server.kt
@@ -1,17 +1,20 @@
 package com.salesground.speedforce.localnetwork
 
+import com.salesground.speedforce.communicationprotocol.FileTransferProtocol
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.net.ServerSocket
 import java.net.Socket
 
 class Server {
 
-    suspend fun listenIncomingConnection() {
+    suspend fun listenIncomingConnection(filesToTransfer: MutableList<File>) {
         withContext(Dispatchers.IO) {
             val serverSocket: ServerSocket = ServerSocket(8090)
             // blocking call
             val client: Socket = serverSocket.accept()
+            FileTransferProtocol(client).transferFile(filesToTransfer = filesToTransfer)
         }
     }
 }
