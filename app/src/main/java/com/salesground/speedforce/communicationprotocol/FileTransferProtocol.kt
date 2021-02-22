ce2884ecf8ec27e1034573c11c37aa66a84214f2
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 15:05:25 +0100

    Building the transferFile and receiveFile functions in the FileTransferProtocol class

--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FileTransferProtocol.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FileTransferProtocol.kt
@@ -1,15 +1,26 @@
 package com.salesground.speedforce.communicationprotocol
 
-import java.io.File
+import java.io.*
 import java.net.Socket
 
 class FileTransferProtocol(private val socket: Socket) {
 
-    fun transferFiles(filesToTransfer : MutableList<File>){
+    fun transferFile(filesToTransfer: MutableList<File>) {
+        val socketOutputStream = socket.getOutputStream()
+        val socketBOS = BufferedOutputStream(socketOutputStream)
+        val socketDOS = DataOutputStream(socketBOS)
 
+        // write the number of files to transer
+        socketDOS.writeInt(filesToTransfer.size)
+
+        // write the name and length of each file to transfer
+        filesToTransfer.forEach { file ->
+            socketDOS.writeUTF(file.name)
+            socketDOS.writeLong(file.length())
+        }
     }
 
-    fun receiveFiles(){
+    fun receiveFile() {
 
     }
 }
