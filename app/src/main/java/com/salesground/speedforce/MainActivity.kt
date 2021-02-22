057ae0bc08f8c4550024663ded3222606570531a
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 11:53:14 +0100

    Created function to initialize wifiP2pChannel and wifiDirectBroadcastReceiver variables

--- app/src/main/java/com/salesground/speedforce/MainActivity.kt
+++ app/src/main/java/com/salesground/speedforce/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.salesground.speedforce
 
 import android.Manifest.*
+import android.content.Context
 import android.content.pm.PackageManager
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -14,12 +16,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.salesground.speedforce.broadcast.WifiDirectBroadcastReceiver
 import com.salesground.speedforce.ui.theme.SpeedForceTheme
 import com.salesground.speedforce.viewmodel.MainActivityViewModel
 
 private const val FINE_LOCATION_REQUEST_CODE = 100
+
 class MainActivity : AppCompatActivity() {
-    val mainActivityViewModel by viewModels<MainActivityViewModel>()
+    private val mainActivityViewModel by viewModels<MainActivityViewModel>()
+    private val wifiP2pManager: WifiP2pManager by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+    }
+    private lateinit var wifiP2pChannel: WifiP2pManager.Channel
+    private lateinit var wifiDirectBroadcastReceiver: WifiDirectBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,35 +41,54 @@ class MainActivity : AppCompatActivity() {
             }
         }
         observeViewModelLiveData()
-
-
+        initializeChannelAndBroadcastReceiver()
     }
 
-    fun observeViewModelLiveData(){
+    private fun initializeChannelAndBroadcastReceiver() {
+        wifiP2pChannel =
+            wifiP2pManager.initialize(this, mainLooper, object : WifiP2pManager.ChannelListener {
+                // The channel to the framework has been disconnected.
+                // Application could try re-initializing
+                override fun onChannelDisconnected() {
+                }
+            })
+        // use the activity, wifiP2pManager and wifiP2pChannel to initialize the wifiDiectBroadcastReceiver
+        wifiP2pChannel.also { channel: WifiP2pManager.Channel ->
+            wifiDirectBroadcastReceiver = WifiDirectBroadcastReceiver(
+                mainActivity = this,
+                wifiP2pManager = wifiP2pManager,
+                wifiP2pChannel = wifiP2pChannel
+            )
+        }
+    }
+
+    fun observeViewModelLiveData() {
         // wifiP2p state changed, either enabled or disabled
         mainActivityViewModel.isWifiP2pEnabled.observe(this, Observer {
             it?.let {
-
+                if (it) {
+                    // begin peer discovery
+                }
             }
         })
     }
 
-    fun wifiP2pState(isEnabled : Boolean){
+    fun wifiP2pState(isEnabled: Boolean) {
         mainActivityViewModel.wifiP2pStateChange(newState = isEnabled)
     }
 
     // check if SpeedForce has access to device fine location
-    private fun checkFineLocationPermission(){
+    private fun checkFineLocationPermission() {
         val isFineLocationPermissionGranted = ContextCompat.checkSelfPermission(
             this,
             permission.ACCESS_FINE_LOCATION
         ) ==
                 PackageManager.PERMISSION_GRANTED
 
-        if(isFineLocationPermissionGranted){
+        if (isFineLocationPermissionGranted) {
             // TODO check if the device location is on, using location manager
             //TODO more resource @ https://developer.android.com/training/location
-        }else {
+        } else {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(permission.ACCESS_FINE_LOCATION),
@@ -73,9 +102,9 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if(requestCode== FINE_LOCATION_REQUEST_CODE && permissions.contains(permission.ACCESS_FINE_LOCATION)){
-            if(grantResults.isNotEmpty()){
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == FINE_LOCATION_REQUEST_CODE && permissions.contains(permission.ACCESS_FINE_LOCATION)) {
+            if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // access to device fine location has been granted to SpeedForce
                     // TODO check if the device location is on, using location manager
                     //TODO more resource @ https://developer.android.com/training/location
