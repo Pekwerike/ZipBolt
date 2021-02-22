0e70a2d95b1e9b8312c4a02978a6607b8a65c4c6
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 20:36:53 +0100

    Completed ImageModel data class

--- app/src/main/java/com/salesground/speedforce/model/ImageModel.kt
+++ app/src/main/java/com/salesground/speedforce/model/ImageModel.kt
@@ -2,5 +2,8 @@ package com.salesground.speedforce.model
 
 import android.net.Uri
 
-data class ImageModel (val imageUri : Uri
+data class ImageModel (val imageUri : Uri,
+                       val imageDisplayName : String,
+                       val imageDateAdded : Long,
+                       val imageSize : Long
 )
