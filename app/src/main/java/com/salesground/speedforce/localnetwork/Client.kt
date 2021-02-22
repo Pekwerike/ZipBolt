32e76bd9e5b876a86fe865cacd7d94c21ab95aad
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 14:37:24 +0100

    Creating Client and Server configuration in the Local Area Network

--- app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
+++ app/src/main/java/com/salesground/speedforce/localnetwork/Client.kt
@@ -1,4 +1,15 @@
 package com.salesground.speedforce.localnetwork
 
-class Client {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.Socket
+
+class Client(private val serverIpAddress : String) {
+
+   suspend fun connectToServer() {
+       withContext(Dispatchers.IO) {
+           val server: Socket = Socket(serverIpAddress, 8090)
+
+       }
+   }
 }
--- app/src/main/java/com/salesground/speedforce/localnetwork/Server.kt
+++ app/src/main/java/com/salesground/speedforce/localnetwork/Server.kt
@@ -1,4 +1,16 @@
 package com.salesground.speedforce.localnetwork
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.ServerSocket
+
 class Server {
+
+    suspend fun listenIncomingConnection() {
+        withContext(Dispatchers.IO) {
+            val serverSocket: ServerSocket = ServerSocket(8090)
+            // blocking call
+            val client = serverSocket.accept()
+        }
+    }
 }
