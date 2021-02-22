79499d7891c1ece2ad4490bf78bd41fd9784e681
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 10:38:37 +0100

    Created checkFineLocationPermission function to check if SpeedForce has permission to use the device location

--- app/src/main/java/com/salesground/speedforce/MainActivity.kt
+++ app/src/main/java/com/salesground/speedforce/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.salesground.speedforce
 
+import android.Manifest.*
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
@@ -8,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.salesground.speedforce.ui.theme.SpeedForceTheme
 
+private const val FINE_LOCATION_REQUEST_CODE = 100
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +21,30 @@ class MainActivity : AppCompatActivity() {
             SpeedForceTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    // check if SpeedForce has access to device fine location
+    private fun checkFineLocationPermission(){
+        val isFineLocationPermissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            permission.ACCESS_FINE_LOCATION
+        ) ==
+                PackageManager.PERMISSION_GRANTED
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpeedForceTheme {
-        Greeting("Android")
+        if(isFineLocationPermissionGranted){
+
+        }else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(permission.ACCESS_FINE_LOCATION),
+                FINE_LOCATION_REQUEST_CODE
+            )
+        }
     }
 }
+
+
