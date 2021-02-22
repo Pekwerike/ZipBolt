e66f6d674f69b239c20e5299df6295d8f53a66aa
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 11:24:47 +0100

    Building WifiDirectBroadcastReceiver class

--- app/src/main/java/com/salesground/speedforce/broadcast/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/salesground/speedforce/broadcast/WifiDirectBroadcastReceiver.kt
@@ -13,6 +13,32 @@ class WifiDirectBroadcastReceiver(
 ) : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
+        p1?.let { intent: Intent ->
+            val action: String? = intent.action
+            action?.let {
+                when (it) {
+                    // Broadcast when Wi-Fi P2P is enabled or disabled on the device.
+                    WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
 
+                    }
+
+                    /* Broadcast when you call discoverPeers().
+                        You will usually call requestPeers() to get an updated list of peers if you handle this intent in your application.
+                        */
+                    WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                    }
+
+                    //Broadcast when the state of the device's Wi-Fi connection changes.
+                    WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+
+                    }
+
+                    // Broadcast when a device's details have changed, such as the device's name.
+                    WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+
+                    }
+                }
+            }
+        }
     }
 }
