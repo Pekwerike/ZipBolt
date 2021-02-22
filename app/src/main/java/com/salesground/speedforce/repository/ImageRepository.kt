b70c23a35d26b2749c3ef4e0f5f1ddc934ab59e6
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 20:29:14 +0100

    Building ImageRepository

--- app/src/main/java/com/salesground/speedforce/repository/ImageRepository.kt
+++ app/src/main/java/com/salesground/speedforce/repository/ImageRepository.kt
@@ -1,8 +1,42 @@
 package com.salesground.speedforce.repository
 
-class ImageRepository {
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 
-    fun fetchAllImagesOnDevice(){
+class ImageRepository(private val applicationContext: Context) {
+
+    fun fetchAllImagesOnDevice() {
+        val collection: Uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL_PRIMARY
+            )
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val projection: Array<String> = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.SIZE
+        )
+
+        val selection = null
+        val selectionArgs = null
+        val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} ASC"
+
+        applicationContext.contentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.apply {cursor : Cursor ->
+            val imageIdIndex =
+        }
 
     }
 }
