977547b9a6d96a8456387b741c8fc219f04b2d1a
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-22 15:23:14 +0100

    Building FileTransferProtocol

--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FileTransferProtocol.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FileTransferProtocol.kt
@@ -1,5 +1,6 @@
 package com.salesground.speedforce.communicationprotocol
 
+import androidx.collection.ArrayMap
 import java.io.*
 import java.net.Socket
 
@@ -18,9 +19,35 @@ class FileTransferProtocol(private val socket: Socket) {
             socketDOS.writeUTF(file.name)
             socketDOS.writeLong(file.length())
         }
+
+        // write the byte of each file to the socketDOS
+        filesToTransfer.forEach { file: File ->
+            val fileInputStream = FileInputStream(file)
+            val buffer = ByteArray(1_000_000)
+            var length: Int
+            while (fileInputStream.read(buffer).also {
+                    length = it
+                } != -1) {
+                socketDOS.write(buffer, 0,length)
+            }
+        }
     }
 
     fun receiveFile() {
+        val socketInputStream = socket.getInputStream()
+        val socketBIS = BufferedInputStream(socketInputStream)
+        val socketDIS = DataInputStream(socketBIS)
+        val filesReceived: ArrayMap<String, Long> = ArrayMap()
+
+        // read the number of files sent
+        val filesCount = socketDIS.readInt()
+
+        // read the name and length of each file sent
+        for (i in 0 until filesCount) {
+            val fileName = socketDIS.readUTF()
+            val fileLength = socketDIS.readLong()
+            filesReceived.put(fileName, fileLength)
+        }
 
     }
 }
