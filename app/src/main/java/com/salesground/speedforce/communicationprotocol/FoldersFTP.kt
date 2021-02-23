7574fc7d28bbf63fbfea21a90cb5eb3654449a74
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-23 14:36:20 +0100

    Building transferFolder function in FoldersFTP

--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
@@ -1,4 +1,22 @@
 package com.salesground.speedforce.communicationprotocol
 
-class FoldersFTP {
+import java.io.File
+import java.net.Socket
+
+class FoldersFTP(private val socket: Socket) {
+    private var filesCount = 0
+    private val filesDTO : MutableList<FileDTO> = mutableListOf()
+    fun transferFolder(folder : File){
+
+    }
+
+    fun folderCount(file : File){
+        filesCount += 1
+
+        if(file.isDirectory){
+            val filesInDirectory = file.listFiles()
+
+        }
+
+    }
 }
