7d78a8b59328b64f8e1dd1ba05088a1a0e56f76c
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-23 18:34:27 +0100

    Building ZipBolt temporary UI

--- app/src/main/java/com/salesground/zipbolt/ui/screen/SpeedForceTempHomepage.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/SpeedForceTempHomepage.kt
@@ -1,24 +1,47 @@
 package com.salesground.zipbolt.ui.screen
 
 import android.net.wifi.p2p.WifiP2pDevice
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import com.salesground.zipbolt.viewmodel.MainActivityViewModel
 
 @Composable
-fun HomeScreen(mainActivityViewModel: MainActivityViewModel) {
+fun HomeScreen(
+    mainActivityViewModel: MainActivityViewModel,
+    sendAction: () -> Unit, receiveAction: () -> Unit
+) {
+
     val listOfDiscoveredDevices = mainActivityViewModel.discoveredPeersListState
-    LazyColumn(content = {
-        items(listOfDiscoveredDevices.value) { device ->
-            DiscoveredPeerUI(device = device)
+    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+        LazyColumn(content = {
+            items(listOfDiscoveredDevices.value) { device ->
+                DiscoveredPeerUI(device = device)
+            }
+        })
+
+        Row() {
+            Button(onClick = sendAction) {
+                Text(text = "Send")
+            }
+            Button(onClick = receiveAction) {
+                Text(text = "Receive")
+            }
         }
-    })
+    }
+
 }
 
 @Composable
