2b0d7c758639c6e216b6c954131ce721e33888b4
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-23 14:46:40 +0100

    Added more descriptive comments to the paramemters for FileDTO class

--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FileDTO.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FileDTO.kt
@@ -2,6 +2,13 @@ package com.salesground.speedforce.communicationprotocol
 
 import java.io.File
 
+/**
+ * Parameters
+ * 1. name -> Single file or folder name
+ * 2. childCount -> folders have a childCount > 0, while single files have a count equal to 0
+ * 3. length -> folders have a file length of 0, while single files have a length > 0
+ * 4. file -> raw file to be transferred, for folders, file = null, while for single files, file has an actual value
+ */
 data class FileDTO(
     val name: String,
     val childCount: Long,
--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
@@ -1,5 +1,7 @@
 package com.salesground.speedforce.communicationprotocol
 
+import java.io.BufferedOutputStream
+import java.io.DataOutputStream
 import java.io.File
 import java.net.Socket
 
@@ -9,20 +11,30 @@ class FoldersFTP(private val socket: Socket) {
 
     fun transferFolder(folder : File){
         val socketOS = socket.getOutputStream()
-        folderCount(folder)
+        val socketBOS = BufferedOutputStream(socketOS)
+        val socketDOS = DataOutputStream(socketBOS)
+
+        folderCount(folder) // get the total number of files to send and group both folders and files into a filesDTO
+
+        socketDOS.write(filesCount) // number of files to send
+
 
     }
 
     fun folderCount(file : File){
+        // increment the files count by one, for a new file
         filesCount += 1
 
         if(file.isDirectory){
             val filesInDirectory = file.listFiles()!!
+            // add directory to the list of filesDTO
             filesDTO.add(FileDTO(name = file.name, childCount = filesInDirectory.size.toLong(), length = 0, file = null ))
+            // for every file in the directory, count them, and add them to the list of filesDTO
             filesInDirectory.forEach{
                 folderCount(it)
             }
         }else {
+            // add file directory to the list of DTO
             filesDTO.add(FileDTO(name = file.name, childCount =  0, length = file.length(), file = file))
         }
     }
