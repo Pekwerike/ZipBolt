c0ef574bd07cd0a109f422bbeb99d21d196ac102
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-23 15:05:03 +0100

    Completed receiveFolder function in FoldersFTP

--- app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
+++ app/src/main/java/com/salesground/speedforce/communicationprotocol/FoldersFTP.kt
@@ -1,10 +1,8 @@
 package com.salesground.speedforce.communicationprotocol
 
-import java.io.BufferedOutputStream
-import java.io.DataOutputStream
-import java.io.File
-import java.io.FileInputStream
+import java.io.*
 import java.net.Socket
+import kotlin.math.min
 
 class FoldersFTP(private val socket: Socket) {
     private var filesCount = 0
@@ -37,9 +35,45 @@ class FoldersFTP(private val socket: Socket) {
         }
     }
 
-    fun receiveFolder(){
+    fun receiveFolder(parentFolder : File){
         val socketIS = socket.getInputStream()
+        val socketBIS = BufferedInputStream(socketIS)
+        val socketDIS = DataInputStream(socketBIS)
+
+        filesCount = socketDIS.read()
+
+        while(filesCount > 0){
+            readFiles(parentFolder, socketDIS)
+        }
+        socketDIS.close()
     }
+
+    private fun readFiles(parentFolder : File, socketDIS : DataInputStream){
+        val fileName = socketDIS.readUTF()
+
+        if(fileName.endsWith("Folder")){
+            fileName.removeSuffix("Folder")
+            val newParentFolder = File(parentFolder, fileName)
+            if(!newParentFolder.exists()) newParentFolder.mkdirs()
+
+            val childCount = socketDIS.readLong()
+
+            for(i in 0 until childCount){
+                readFiles(newParentFolder, socketDIS)
+            }
+        }else {
+            var lengthOfDataForFile = socketDIS.readLong()
+            val fileOS = FileOutputStream(File(parentFolder, fileName))
+            val bufferArray = ByteArray(5_000_000)
+
+            while(lengthOfDataForFile > 0){
+                val lengthOfDataRead = socketDIS.read(bufferArray)
+                fileOS.write(bufferArray, 0, min(lengthOfDataForFile.toInt(), lengthOfDataRead))
+                lengthOfDataForFile -= lengthOfDataRead
+            }
+        }
+    }
+
 
     private fun folderCount(file : File){
         // increment the files count by one, for a new file
