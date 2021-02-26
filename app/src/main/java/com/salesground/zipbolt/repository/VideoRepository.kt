e330bd8cd53e9cb732705263350e48f0f2f4913e
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 08:41:10 +0100

    Building VideoRepository

--- app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
@@ -1,18 +1,40 @@
 package com.salesground.zipbolt.repository
 
 import android.content.Context
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import com.salesground.zipbolt.model.MediaModel
 
-class VideoRepository (private val context : Context) {
+class VideoRepository(private val context: Context) {
 
-    suspend fun getAllVideoFromDevice() : MutableList<MediaModel>{
-        val collection : Uri = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+    suspend fun getAllVideoFromDevice(): MutableList<MediaModel> {
+        val collection: Uri = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
             MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        }else {
+        } else {
             MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val projection: Array<String> = arrayOf(
+            MediaStore.Video.Media._ID,
+            MediaStore.Video.Media.DISPLAY_NAME,
+            MediaStore.Video.Media.SIZE,
+            MediaStore.Video.Media.DATE_ADDED,
+            MediaStore.Video.Media.MIME_TYPE
+        )
+        val selection = null
+        val selectionArgs = null
+        val sortOrder = "${MediaStore.Video.Media.DATE_ADDED} ASC"
+
+        context.contentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.let { cursor: Cursor ->
+            val
         }
     }
 }
