11ff766c19096722410441769a9565c9a75b7566
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 10:43:13 +0100

    Building VideoRepository

--- app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
@@ -14,9 +14,37 @@ import kotlinx.coroutines.flow.flow
 class VideoRepository(private val context: Context) {
 
     fun getAllVideoFromDeviceAsFlow: Flow<MediaModel> = flow {
-        val collection: Uri = if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
+        val collection: Uri = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
             MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY) else
-                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        val projection: Array<String> = arrayOf(
+            MediaStore.Video.Media._ID,
+            MediaStore.Video.Media.DISPLAY_NAME,
+            MediaStore.Video.Media.SIZE,
+            MediaStore.Video.Media.DATE_ADDED,
+            MediaStore.Video.Media.MIME_TYPE
+        )
+        val selection = null
+        val selectionArguments = null
+        val sortOrder = "${MediaStore.Video.Media.DATE_ADDED} ASC"
+
+        context.contentResolver.query(
+            collection,
+            projection,
+            selection,
+            selectionArguments,
+            sortOrder
+        )?.let {cursor: Cursor ->
+            val videoIdColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+            val videoDisplayNameColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+            val videoSizeColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
+            val videoDateAddedColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED)
+            val videoMimeTypeColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE)
+        }
+
     }
 
     suspend fun getAllVideoFromDevice(): MutableList<MediaModel> {
