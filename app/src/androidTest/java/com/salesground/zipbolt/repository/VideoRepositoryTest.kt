d14ed28687addf4038a6643f3dc455dd44be249b
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 11:54:06 +0100

    Building VideoRepositoryTest

--- app/src/androidTest/java/com/salesground/zipbolt/repository/VideoRepositoryTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/VideoRepositoryTest.kt
@@ -1,14 +1,37 @@
 package com.salesground.zipbolt.repository
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.salesground.zipbolt.model.MediaCategory
+import com.salesground.zipbolt.model.MediaModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class VideoRepositoryTest {
 
+    private val applicationContext = ApplicationProvider.getApplicationContext<Context>()
+    private lateinit var videoRepository: VideoRepository
+
     @Before
     fun setUp() {
+        videoRepository = VideoRepository(applicationContext)
+    }
+
+
+    @Test
+    fun test_getAllVideosFromDeviceAsFlow_emitsValue() = runBlocking {
+        val firstVideoOnDevice: MediaModel = videoRepository.getAllVideoFromDeviceAsFlow().first()
+
+        assertTrue(firstVideoOnDevice.mediaDisplayName.isNullOrEmpty())
+        assertEquals(firstVideoOnDevice.mediaCategory, MediaCategory.VIDEO)
+        assertEquals(firstVideoOnDevice.mimeType, "mp4")
+
     }
 
     @After
