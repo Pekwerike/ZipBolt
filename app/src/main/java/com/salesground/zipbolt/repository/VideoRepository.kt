27759f89062cbd07d226f45eb904d5165d29daf9
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 10:39:06 +0100

    Building getAllVideoFromDeviceAsFlow function in VideoRepository

--- app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
@@ -1,15 +1,27 @@
 package com.salesground.zipbolt.repository
 
+import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import com.salesground.zipbolt.model.MediaCategory
 import com.salesground.zipbolt.model.MediaModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class VideoRepository(private val context: Context) {
 
+    fun getAllVideoFromDeviceAsFlow: Flow<MediaModel> = flow {
+        val collection: Uri = if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
+            MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY) else
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+    }
+
     suspend fun getAllVideoFromDevice(): MutableList<MediaModel> {
+        val collectionOfVideosOnDevice: MutableList<MediaModel> = mutableListOf()
+
         val collection: Uri = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
             MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
         } else {
@@ -35,20 +47,36 @@ class VideoRepository(private val context: Context) {
             sortOrder
         )?.let { cursor: Cursor ->
             val videoIdColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
-            val videoDisplayNameColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
+            val videoDisplayNameColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME)
             val videoSizeColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE)
-            val videoDateAddedColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED)
-            val videoMimeTypeColumnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE)
+            val videoDateAddedColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED)
+            val videoMimeTypeColumnIndex =
+                cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE)
 
-            while(cursor.moveToNext()){
+            while (cursor.moveToNext()) {
                 val videoId = cursor.getLong(videoIdColumnIndex)
                 val videoDisplayName = cursor.getString(videoDisplayNameColumnIndex)
                 val videoSize = cursor.getLong(videoSizeColumnIndex)
                 val videoDateAdded = cursor.getLong(videoDateAddedColumnIndex)
                 val videoMimeType = cursor.getString(videoMimeTypeColumnIndex)
 
+                val videoUri =
+                    ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, videoId)
 
+                collectionOfVideosOnDevice.add(
+                    MediaModel(
+                        mediaUri = videoUri,
+                        mediaDisplayName = videoDisplayName,
+                        mediaDateAdded = videoDateAdded,
+                        mediaSize = videoSize,
+                        mediaCategory = MediaCategory.VIDEO,
+                        mimeType = videoMimeType
+                    )
+                )
             }
         }
+        return collectionOfVideosOnDevice
     }
 }
