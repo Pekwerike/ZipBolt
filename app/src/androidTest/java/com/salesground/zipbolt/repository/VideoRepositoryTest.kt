ff025de4b2e99b3d520cd1fb20283b5400777d5c
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 10:46:07 +0100

    Created VideoRepositoryTest class

---
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/VideoRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.salesground.zipbolt.repository
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class VideoRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
