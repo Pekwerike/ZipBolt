e510591eae42c8ca31b5ecbf12fe95f40bcf0aa5
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-26 08:37:41 +0100

    Building VideoRepository

--- app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/VideoRepository.kt
@@ -1,10 +1,18 @@
 package com.salesground.zipbolt.repository
 
 import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import com.salesground.zipbolt.model.MediaModel
 
 class VideoRepository (private val context : Context) {
 
-    suspend fun getAllVideoFromDevice() : Flow{
-
+    suspend fun getAllVideoFromDevice() : MutableList<MediaModel>{
+        val collection : Uri = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        }else {
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        }
     }
 }
