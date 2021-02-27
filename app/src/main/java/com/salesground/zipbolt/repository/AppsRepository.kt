f2d6ed4cd238d7e733c6ff357fe72c809a45f0f9
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-27 11:32:40 +0100

    Building AppsRepository and added permissions for Query device package manager in the AndroidManifest.xml file

--- app/src/main/java/com/salesground/zipbolt/repository/AppsRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/AppsRepository.kt
@@ -1,2 +1,17 @@
 package com.salesground.zipbolt.repository
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+
+
+public class AppsRepository(private val context : Context){
+
+
+    fun getAllAppsOnDevice(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.packageManager.getInstalledApplications(PackageManager.INSTALL_REASON_USER)
+        }
+    }
+}
