08d9cb75501dd889b13ef3e4480d3faf24be41f3
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-27 21:10:53 +0100

    Building AudioRepository

--- app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
@@ -11,6 +11,25 @@ class AudioRepository(private val context: Context) {
             MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY) else
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
 
+        val projection : Array<String> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            arrayOf(MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.MIME_TYPE,
+            MediaStore.Audio.Media.DATE_ADDED,
+            MediaStore.Audio.Media.SIZE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM)
+        } else {
+            arrayOf(MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.MIME_TYPE,
+                MediaStore.Audio.Media.DATE_ADDED,
+                MediaStore.Audio.Media.SIZE)
+        }
+
+
+
+
     }
 
 }
