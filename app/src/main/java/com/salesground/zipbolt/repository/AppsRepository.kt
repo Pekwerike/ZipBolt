87ca1881aa33030225cfeee580e28eb30e40fbfb
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-27 11:37:58 +0100

    Building AppsRepository

--- app/src/main/java/com/salesground/zipbolt/repository/AppsRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/AppsRepository.kt
@@ -2,16 +2,16 @@ package com.salesground.zipbolt.repository
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Build
 
 
-public class AppsRepository(private val context : Context){
+public class AppsRepository(private val context: Context) {
 
 
-    fun getAllAppsOnDevice(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.packageManager.getInstalledApplications(PackageManager.INSTALL_REASON_USER)
-        }
+    fun getAllAppsOnDevice(): MutableList<ApplicationInfo> {
+        return context.packageManager.getInstalledApplications(0)
     }
+
 }
