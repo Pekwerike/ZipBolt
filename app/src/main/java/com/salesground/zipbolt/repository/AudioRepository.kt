6bafdb31abf0c1a5857212b7a4c541d1b0a8f0f0
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-27 21:04:40 +0100

    Building AudioRepository

--- app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
@@ -1,4 +1,16 @@
 package com.salesground.zipbolt.repository
 
-class AudioRepository {
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+
+class AudioRepository(private val context: Context) {
+
+    fun getAllAudioFilesOnDevice() {
+        val collection = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
+            MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY) else
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+    }
+
 }
