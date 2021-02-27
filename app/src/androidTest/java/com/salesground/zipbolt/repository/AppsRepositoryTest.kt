4c3564f601e0534436b25d8abdf9e73ca1730914
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-27 11:42:47 +0100

    Setup AppsRepositoryTest

--- app/src/androidTest/java/com/salesground/zipbolt/repository/AppsRepositoryTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/AppsRepositoryTest.kt
@@ -1,12 +1,18 @@
 package com.salesground.zipbolt.repository
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import org.junit.Before
 
 import org.junit.Assert.*
 
 class AppsRepositoryTest {
 
+    private val applicationContext = ApplicationProvider.getApplicationContext<Context>()
+    private lateinit var appsRepository: AppsRepository
+
     @Before
     fun setUp() {
+        appsRepository = AppsRepository(applicationContext)
     }
 }
