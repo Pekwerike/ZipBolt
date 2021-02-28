2a274fd4b5312a784ca1cadb780a28685f25d118
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-28 18:57:58 +0100

    Bug fix

--- app/src/main/java/com/salesground/zipbolt/extensions/FileExtensions.kt
+++ app/src/main/java/com/salesground/zipbolt/extensions/FileExtensions.kt
@@ -1,2 +1,15 @@
 package com.salesground.zipbolt.extensions
 
+import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+
+fun Uri.getMediaDuration(context: Context): String {
+    var mediaDuration: String? = ""
+    MediaMetadataRetriever().also { mediaMetaDataRetriver : MediaMetadataRetriever ->
+        mediaMetaDataRetriver.setDataSource(context, this)
+        mediaDuration = mediaMetaDataRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+        mediaMetaDataRetriver.release()
+    }
+    return mediaDuration ?: ""
+}
