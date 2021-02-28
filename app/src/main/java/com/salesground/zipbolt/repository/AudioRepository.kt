9763e8230b799c41d2845b474ac1d22a0d831351
Prosper Ekwerike <pekwerike@gmail.com>
2021-02-28 18:33:25 +0100

    Completed getMediaDurationFunction in FilesUtil

--- app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/AudioRepository.kt
@@ -5,10 +5,12 @@ import android.content.Context
 import android.database.Cursor
 import android.os.Build
 import android.provider.MediaStore
+import com.salesground.zipbolt.model.MediaModel
+import kotlinx.coroutines.flow.Flow
 
 class AudioRepository(private val context: Context) {
 
-    fun getAllAudioFilesOnDevice() : Flow<Audi> {
+    fun getAllAudioFilesOnDevice() : Flow<MediaModel> {
         val collection = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q)
             MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY) else
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
--- app/src/main/java/com/salesground/zipbolt/utils/FileUtils.kt
+++ app/src/main/java/com/salesground/zipbolt/utils/FileUtils.kt
@@ -1,7 +1,14 @@
 package com.salesground.zipbolt.utils
 
 import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.net.Uri
 
-fun getMediaDuration(context : Context){
-
+fun getMediaDuration(context: Context, mediaUri: Uri): String {
+    var mediaDuration: String? = ""
+    MediaMetadataRetriever().apply {
+        this.setDataSource(context, mediaUri)
+        mediaDuration = this.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+    }
+    return mediaDuration ?: ""
 }
