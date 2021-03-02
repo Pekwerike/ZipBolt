2a905155b252893ab80dcaa46ad7264e239f6036
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-02 08:59:31 +0100

    Created FileTransferService class

---
+++ app/src/main/java/com/salesground/zipbolt/foregroundservice/FileTransferService.kt
@@ -0,0 +1,12 @@
+package com.salesground.zipbolt.foregroundservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FileTransferService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
