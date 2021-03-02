4b35075863f469ed185c52ebba61175a140f803d
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-02 09:46:16 +0100

    Added some key constants for the ClientService and ServerService notification in the strings.xml file

--- app/src/main/java/com/salesground/zipbolt/foregroundservice/ClientService.kt
+++ app/src/main/java/com/salesground/zipbolt/foregroundservice/ClientService.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.notification.FILE_TRANSFER_SERVICE_NOTIFICATION_ID
 
 class ClientService : Service() {
     private val clientServiceBinder = ClientServiceBinder()
@@ -17,7 +19,11 @@ class ClientService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val serviceNotification = NotificationCompat.Builder(this, )
+        val serviceNotification = NotificationCompat.Builder(this, FILE_TRANSFER_SERVICE_NOTIFICATION_ID)
+            .apply {
+                setContentTitle(getString(R.string.fileTransferServiceNotificationTitle))
+                setContentText(getString(R.string.))
+            }
 
         return START_NOT_STICKY
     }
