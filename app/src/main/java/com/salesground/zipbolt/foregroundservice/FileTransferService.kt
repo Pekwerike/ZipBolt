dc9decb4b86dd712242305d6f4a81cc84b684328
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-02 09:02:39 +0100

    Building FileTransferService

--- app/src/main/java/com/salesground/zipbolt/foregroundservice/FileTransferService.kt
+++ app/src/main/java/com/salesground/zipbolt/foregroundservice/FileTransferService.kt
@@ -2,11 +2,23 @@ package com.salesground.zipbolt.foregroundservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 
 class FileTransferService : Service() {
+    private val fileTransferServiceBinder = FileTransferServiceBinder()
 
+    inner class FileTransferServiceBinder : Binder() {
+        fun getFileTransferService() = this@FileTransferService
+    }
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return fileTransferServiceBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val serviceNotification = NotificationCompat.Builder(this, )
+
+        return START_NOT_STICKY
     }
 }
