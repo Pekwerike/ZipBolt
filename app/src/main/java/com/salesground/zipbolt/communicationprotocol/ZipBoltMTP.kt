afb30186b94c27d054f839a3311eb029f4d54748
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-02 18:10:40 +0100

    Building ZipBoltMTP

--- app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMTP.kt
+++ app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMTP.kt
@@ -1,11 +1,15 @@
 package com.salesground.zipbolt.communicationprotocol
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Environment
 import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
 import com.salesground.zipbolt.model.MediaCategory
 import com.salesground.zipbolt.model.MediaModel
 import java.io.DataInputStream
 import java.io.DataOutputStream
+import java.io.File
 import java.io.FileInputStream
 
 class ZipBoltMTP(private val context: Context) {
@@ -49,9 +53,37 @@ class ZipBoltMTP(private val context: Context) {
             val mediaType = DIS.readUTF()
 
             // read media bytes and save it into the media store based on the mime type
-            when(mediaType) {
+            when (mediaType) {
                 MediaCategory.VIDEO.name -> TODO()
-                MediaCategory.IMAGE.name -> TODO()
+                MediaCategory.IMAGE.name -> {
+                    val parentRelativePath =
+                        File(Environment.getExternalStorageDirectory(), "ZipBolt")
+                    if (!parentRelativePath.exists()) parentRelativePath.mkdirs()
+                    val imagesRelativePath = File(parentRelativePath, "images")
+                    if (!imagesRelativePath.exists()) imagesRelativePath.mkdirs()
+                    val imageFile = File(imagesRelativePath, mediaName)
+
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.Images.Media.DISPLAY_NAME, mediaName)
+                        put(MediaStore.Images.Media.OWNER_PACKAGE_NAME, context.packageName)
+                        put(MediaStore.Images.Media.RELATIVE_PATH, imagesRelativePath.absolutePath)
+                        put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+                        put(MediaStore.Images.Media.SIZE, mediaSize)
+                        put(MediaStore.Images.Media.MIME_TYPE, "image/*")
+                        put(
+                            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+                            imagesRelativePath.absolutePath
+                        )
+                        put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis())
+                        put(MediaStore.Images.Media.IS_PENDING, 1)
+                    }
+
+                    val imageUri = context.contentResolver.insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                    )
+
+                }
                 MediaCategory.AUDIO.name -> TODO()
             }
 
