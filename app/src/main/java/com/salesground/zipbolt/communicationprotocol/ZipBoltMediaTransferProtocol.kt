90d8f128cbc757c994a86749b245f208074035be
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-02 12:36:26 +0100

    Building transferMedia function in ZipBoltMediaTransferProtocol

--- app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
+++ app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
@@ -1,10 +1,17 @@
 package com.salesground.zipbolt.communicationprotocol
 
+import android.content.Context
 import com.salesground.zipbolt.model.MediaModel
+import java.io.DataOutputStream
 
-class ZipBoltMediaTransferProtocol {
+class ZipBoltMediaTransferProtocol(private val context: Context) {
 
-    fun transferMedia(mediaItems : MutableList<MediaModel>){
-
+    fun transferMedia(mediaItems : MutableList<MediaModel>, DOS : DataOutputStream){
+        DOS.writeInt(mediaItems.size)
+        mediaItems.forEach { mediaModel : MediaModel ->
+            DOS.writeUTF(mediaModel.mediaDisplayName)
+            DOS.writeLong(mediaModel.mediaSize)
+            context.
+        }
     }
 }
