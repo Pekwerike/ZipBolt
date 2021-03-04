fc6ccc869cec82165d60ce36a32f999714ac753d
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-04 18:57:34 +0100

    Created Homescreen file

---
+++ app/src/main/java/com/salesground/zipbolt/ui/temporaryscreens/Homescreen.kt
@@ -0,0 +1,9 @@
+package com.salesground.zipbolt.ui.temporaryscreens
+
+import androidx.compose.runtime.Composable
+import com.salesground.zipbolt.viewmodel.MainActivityViewModel
+
+@Composable
+fun HomeScreen(mainActivityViewModel: MainActivityViewModel){
+    mainActivityViewModel.
+}
