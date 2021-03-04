91bb2bbef805116c44d423a0a93c0863c74da14d
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-04 18:18:40 +0100

    Added Chris Banes accompanist library for utils composables

--- app/src/main/java/com/salesground/zipbolt/ui/temporaryscreens/DeviceImagesDisplay.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/temporaryscreens/DeviceImagesDisplay.kt
@@ -1,2 +1,14 @@
 package com.salesground.zipbolt.ui.temporaryscreens
 
+import androidx.compose.runtime.Composable
+import com.salesground.zipbolt.model.MediaModel
+
+@Composable
+fun ImagesOnDeviceList(){
+
+}
+
+@Composable
+fun SingleImageOnDevice(image : MediaModel){
+
+}
