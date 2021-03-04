9b06f3ba3932fff4f2cc390a3b109fd6b4b644fd
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-04 11:17:57 +0100

    Building ServerService

--- app/src/main/java/com/salesground/zipbolt/foregroundservice/ServerService.kt
+++ app/src/main/java/com/salesground/zipbolt/foregroundservice/ServerService.kt
@@ -9,13 +9,17 @@ import com.salesground.zipbolt.model.MediaModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.net.ServerSocket
-import java.net.Socket
 
 class ServerService : Service() {
     private val serverServiceBinder: ServerServiceBinder = ServerServiceBinder()
     private lateinit var zipBoltMTP: ZipBoltMTP
-    private var mediaItemsToTransfer : MutableList<MediaModel> = mutableListOf()
+    private var mediaItemsToTransfer: MutableList<MediaModel> = mutableListOf()
+    private var isDataAvailableToTransfer: Boolean = false
 
     override fun onCreate() {
         super.onCreate()
@@ -38,14 +42,44 @@ class ServerService : Service() {
         intent?.let {
             CoroutineScope(Dispatchers.IO).launch {
                 val server = ServerSocket(SOCKET_PORT)
-                server.accept()
+                val client = server.accept()
+                val socketDOS = DataOutputStream(BufferedOutputStream(client.getOutputStream()))
+                val socketDIS = DataInputStream(BufferedInputStream(client.getInputStream()))
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    listenForNewMediaCollectionToTransfer(socketDOS)
+                }
+                CoroutineScope(Dispatchers.IO).launch {
+                    
+                }
             }
         }
 
         return START_NOT_STICKY
     }
+    
+    private suspend fun listenForIncomingMediaItemsToReceive(DIS: DataInputStream){
+        while(true){
+            val isMediaAvailable = DIS.readUTF()
+            if(isMediaAvailabl)
+        }
+    }
 
-    fun transferMediaItems(mediaItems : MutableList<MediaModel>){
+    private suspend fun listenForNewMediaCollectionToTransfer(DOS: DataOutputStream) {
+
+        while (true) {
+            if (isDataAvailableToTransfer) {
+                DOS.writeUTF(DATA_AVAILABLE)
+                zipBoltMTP.transferMedia(mediaItemsToTransfer, DOS)
+                isDataAvailableToTransfer = false
+            } else {
+                DOS.writeUTF(NO_DATA_AVAILABLE)
+            }
+        }
+    }
+
+    fun transferMediaItems(mediaItems: MutableList<MediaModel>) {
         mediaItemsToTransfer = mediaItems
+        isDataAvailableToTransfer = true
     }
 }
