796e485b2fd3dd3f1864b1c97df4897f1d97485d
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-04 10:51:46 +0100

    Made bound configuration for ServerService

--- app/src/main/java/com/salesground/zipbolt/foregroundservice/ServerService.kt
+++ app/src/main/java/com/salesground/zipbolt/foregroundservice/ServerService.kt
@@ -2,13 +2,20 @@ package com.salesground.zipbolt.foregroundservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
 class ServerService : Service() {
+    private val serverServiceBinder : ServerServiceBinder = ServerServiceBinder()
+
+    inner class ServerServiceBinder : Binder() {
+        fun getServerServiceInstance(): ServerService {
+            return this@ServerService
+        }
+    }
 
     override fun onBind(intent: Intent): IBinder {
-
-
+        return serverServiceBinder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
