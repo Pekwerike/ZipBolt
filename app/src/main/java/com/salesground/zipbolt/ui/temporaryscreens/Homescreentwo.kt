32c7bb0fc330eae55589d2c5197b1112da1b6112
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-06 11:48:21 +0100

    Building second temporary homescreen to allow users discover, connect and share selected images

---
+++ app/src/main/java/com/salesground/zipbolt/ui/temporaryscreens/Homescreentwo.kt
@@ -0,0 +1,16 @@
+package com.salesground.zipbolt.ui.temporaryscreens
+
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+
+@ExperimentalMaterialApi
+@Composable
+fun HomeScreenTwo(){
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+    BottomSheetScaffold(sheetContent = { }) {
+
+    }
+}
+
