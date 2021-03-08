7c406175034b7a2341cc87e1915119f8fcc39999
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-08 20:24:37 +0100

    Completed second test case for ZipBoltSavedFilesRepositoryTest class

--- app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltSavedFilesRepositoryTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltSavedFilesRepositoryTest.kt
@@ -7,13 +7,27 @@ import org.junit.Test
 
 class ZipBoltSavedFilesRepositoryTest {
     lateinit var zipBoltSavedFilesRepository: ZipBoltSavedFilesRepository
+
     @Before
     fun setUp() {
         zipBoltSavedFilesRepository = ZipBoltSavedFilesRepository()
     }
 
     @Test
-    fun confirmThatTheBaseDirectoryNameIsZipBolt(){
-        zipBoltSavedFilesRepository.getZipBoltMediaCategoryBaseDirectory(ZipBoltMediaCategory.IMAGES_BASE_DIRECTORY)
+    fun confirmThatTheAppRootDirectoryNameIsZipBolt() {
+        val imageBaseDirectory = zipBoltSavedFilesRepository
+            .getZipBoltMediaCategoryBaseDirectory(ZipBoltMediaCategory.IMAGES_BASE_DIRECTORY)
+        assertEquals(imageBaseDirectory.parentFile.name, "ZipBolt")
     }
+
+    @Test
+    fun confirmThatDirectoriesAreCreatedForEachMediaCategory() {
+        val mediaCategory = ZipBoltMediaCategory.values()
+        mediaCategory.forEach {
+            val mediaCategory =
+                zipBoltSavedFilesRepository.getZipBoltMediaCategoryBaseDirectory(it)
+            assertTrue(mediaCategory.exists())
+        }
+    }
+
 }
