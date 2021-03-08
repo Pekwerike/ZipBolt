3b929ee7476660451b095ee6ed9f3f8b432b6a40
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-08 16:07:46 +0100

    Documented ZipBoltSavedFilesRepository

--- app/src/main/java/com/salesground/zipbolt/repository/ZipBoltSavedFilesRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/ZipBoltSavedFilesRepository.kt
@@ -1,5 +1,15 @@
 package com.salesground.zipbolt.repository
-
+/*
+* Overview
+*  This class is responsible for generating the base directories for other repositories like
+* ImageRepository, VideoRepository etc to saved the files of received items before inserting
+* them into the media store of the various category
+*
+* Functions description
+* 1). getZipBoltBaseDirectory() returns the top level ZipBolt directory in the file system
+* 2). getZipBoltMediaCategoryBaseDirectory() returns the mediaType directory under the main directory
+* 3). checkIfDirectory() exists confirm that a given directory exists, if not then it creates it
+* */
 import android.content.Context
 import android.os.Environment
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -20,7 +30,7 @@ class ZipBoltSavedFilesRepository @Inject constructor() {
 
     private fun getZipBoltBaseDirectory(): File {
         val baseDirectory = File(Environment.getExternalStorageDirectory(), ZIP_BOLT_MAIN_DIRECTORY)
-        if (!baseDirectory.exists()) baseDirectory.mkdirs()
+        checkIfDirectoryExist(baseDirectory)
         return baseDirectory
     }
 
