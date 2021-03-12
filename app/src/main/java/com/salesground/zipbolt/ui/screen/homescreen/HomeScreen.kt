5c4e4fe4d3df7a57f75938c9f24ac692cd187d16
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-12 13:23:16 +0100

    Building HomeScreen

--- app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/HomeScreen.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/HomeScreen.kt
@@ -1,8 +1,24 @@
 package com.salesground.zipbolt.ui.screen.homescreen
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import com.salesground.zipbolt.ui.screen.homescreen.homescreencomponents.DeviceMediaCategoryHeader
 
+@ExperimentalMaterialApi
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    val modalBottomSheetState =
+        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    ModalBottomSheetLayout(
+        sheetContent = { },
+        sheetState = modalBottomSheetState
+    ) {
+        LazyColumn(content = {
+            item {
+                DeviceMediaCategoryHeader(categoryName = "App")
+            }
 
+        })
+    }
 }
