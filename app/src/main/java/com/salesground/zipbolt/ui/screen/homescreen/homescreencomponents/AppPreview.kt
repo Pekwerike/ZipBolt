ed92148cc2754d72e9fdfdf7f62eec8c6402a19a
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-12 12:54:45 +0100

    Building ZipBolt homepage

--- app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/homescreencomponents/AppPreview.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/homescreencomponents/AppPreview.kt
@@ -1,8 +1,22 @@
 package com.salesground.zipbolt.ui.screen.homescreen.homescreencomponents
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import coil.request.ImageRequest
+import com.salesground.zipbolt.model.ApplicationModel
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
-fun AppReview(){
+fun AppReview(application: ApplicationModel){
+    val context = LocalContext.current
+    Column {
+        CoilImage(request = ImageRequest.Builder(context).apply {
+            data(application.appIcon)
+        }.build()) {
 
+        }
+        Text(text = application.applicationName ?: "NaN")
+    }
 }
