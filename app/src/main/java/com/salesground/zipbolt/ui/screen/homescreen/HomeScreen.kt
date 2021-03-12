781ba34f8516c2282e4176f27b1ad59b41dad30c
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-12 14:12:54 +0100

    Building HomeScreen

--- app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/HomeScreen.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/homescreen/HomeScreen.kt
@@ -3,13 +3,17 @@ package com.salesground.zipbolt.ui.screen.homescreen
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.salesground.zipbolt.ui.screen.homescreen.homescreencomponents.DeviceMediaCategoryHeader
+import com.salesground.zipbolt.viewmodel.DeviceApplicationViewModel
 
 @ExperimentalMaterialApi
 @Composable
-fun HomeScreen() {
+fun HomeScreen(deviceApplicationViewModel: DeviceApplicationViewModel) {
     val modalBottomSheetState =
         rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val allApplicationsOnDevice by deviceApplicationViewModel.allApplicationsOnDevice.observeAsState()
     ModalBottomSheetLayout(
         sheetContent = { },
         sheetState = modalBottomSheetState
--- app/src/main/java/com/salesground/zipbolt/viewmodel/DeviceApplicationViewModel.kt
+++ app/src/main/java/com/salesground/zipbolt/viewmodel/DeviceApplicationViewModel.kt
@@ -3,10 +3,14 @@ package com.salesground.zipbolt.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.salesground.zipbolt.model.ApplicationModel
 import com.salesground.zipbolt.repository.DeviceApplicationsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,7 +21,16 @@ class DeviceApplicationViewModel @Inject constructor(
     private var _allApplicationsOnDevice = MutableLiveData<List<ApplicationModel>>()
     val allApplicationsOnDevice : LiveData<List<ApplicationModel>> = _allApplicationsOnDevice
 
-    fun getAllApplicationsOnDevice() {
-        _allApplicationsOnDevice.value = deviceApplicationsRepository.getAllDeviceApplication()
+    init{
+        viewModelScope.launch {
+            getAllApplicationsOnDevice()
+        }
+    }
+
+    private suspend fun getAllApplicationsOnDevice() {
+        _allApplicationsOnDevice.value =  viewModelScope.async(Dispatchers.IO) {
+            deviceApplicationsRepository.getAllDeviceApplication()
+        }.await()
+
     }
 }
