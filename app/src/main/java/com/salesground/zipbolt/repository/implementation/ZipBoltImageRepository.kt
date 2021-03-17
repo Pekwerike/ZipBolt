709c39c0224faf382bb78f086c9e8ea429558a2e
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-17 13:38:01 +0100

    Building ZipBoltImageRepository

--- app/src/main/java/com/salesground/zipbolt/repository/implementation/ZipBoltImageRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/implementation/ZipBoltImageRepository.kt
@@ -1,5 +1,6 @@
 package com.salesground.zipbolt.repository.implementation
 
+import android.content.Context
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
@@ -13,7 +14,7 @@ import javax.inject.Inject
 
 class ZipBoltImageRepository @Inject constructor(
     @ApplicationContext
-    applicationContext: ApplicationContext
+    private val applicationContext: Context
 ) : ImageRepository {
     override suspend fun insertImageIntoMediaStore(
         displayName: String,
@@ -26,12 +27,53 @@ class ZipBoltImageRepository @Inject constructor(
 
     override suspend fun getAllImagesOnDevice(): MutableList<DataToTransfer> {
         val deviceImages = mutableListOf<DataToTransfer>()
-        val collection = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+        val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        }else {
+        } else {
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         }
-        val projection = arrayOf()
+        val projection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_MODIFIED
+            )
+        } else {
+            arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.DATE_MODIFIED
+            )
+        }
+
+        val sortOrder = "${MediaStore.Images.Media.DATE_MODIFIED} DESC"
+
+        applicationContext.contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            sortOrder
+        )?.let { cursor ->
+            val imageIdColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val imageDateModifiedColumnIndex =
+                cursor.getColumnIndex(MediaStore.Images.Media.DATE_MODIFIED)
+            val imageBucketNameIndex = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
+            } else {
+                cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+            }
+            while(cursor.moveToNext()){
+                val imageId = cursor.getLong(imageIdColumnIndex)
+                val imageDateModified = cursor.getLong(imageDateModifiedColumnIndex)
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    cursor.getString(imageBucketNameIndex)
+                }else {
+
+                }
+            }
+        }
+
 
         return deviceImages
     }
