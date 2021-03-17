eeaf3e258d663749ae4b8220662e01803dbc9b03
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-17 13:24:45 +0100

    Building getAllImagesOnDevice function

--- app/src/main/java/com/salesground/zipbolt/repository/implementation/ZipBoltImageRepository.kt
+++ app/src/main/java/com/salesground/zipbolt/repository/implementation/ZipBoltImageRepository.kt
@@ -1,6 +1,8 @@
 package com.salesground.zipbolt.repository.implementation
 
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.core.net.toUri
 import com.salesground.zipbolt.model.DataToTransfer
 import com.salesground.zipbolt.repository.ImageRepository
@@ -24,12 +26,21 @@ class ZipBoltImageRepository @Inject constructor(
 
     override suspend fun getAllImagesOnDevice(): MutableList<DataToTransfer> {
         val deviceImages = mutableListOf<DataToTransfer>()
-
+        val collection = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        }else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        val projection = arrayOf()
 
         return deviceImages
     }
 
     override suspend fun getTenImagesOnDevice(): MutableList<DataToTransfer> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMetaDataOfImage(imageUri: Uri): DataToTransfer {
         TODO("Not yet implemented")
     }
 }
