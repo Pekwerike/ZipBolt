c414fb74948a287efa6d047da7fe2cd129aed741
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-17 23:37:13 +0100

    Preparing DeviceMediaViewModel for tests

---
+++ app/src/androidTest/java/com/salesground/zipbolt/viewmodel/DeviceMediaViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.salesground.zipbolt.viewmodel
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+
+@HiltAndroidTest
+class DeviceMediaViewModelTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+
+}
