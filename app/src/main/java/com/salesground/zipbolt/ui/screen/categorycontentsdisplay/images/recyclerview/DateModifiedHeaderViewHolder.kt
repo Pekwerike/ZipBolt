2a1697fcbbc679ce7fc882376e808706cea844af
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-20 00:09:49 +0100

    Building DeviceImageViewHolder

---
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/recyclerview/DateModifiedHeaderViewHolder.kt
@@ -0,0 +1,32 @@
+package com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.databinding.MediaDateModifiedHeaderBinding
+
+class DateModifiedHeaderViewHolder(
+    private val mediaDateModifiedHeaderBinding:
+    MediaDateModifiedHeaderBinding
+) : RecyclerView.ViewHolder(mediaDateModifiedHeaderBinding.root) {
+
+    fun bindDateModified(dateModified: String) {
+      mediaDateModifiedHeaderBinding.apply {
+          this.dateModified = dateModified
+      }
+    }
+
+    companion object {
+        fun createDateModifiedHeaderViewHolder(parent: ViewGroup): DateModifiedHeaderViewHolder {
+            val layoutItemBinding = DataBindingUtil.inflate<MediaDateModifiedHeaderBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.media_date_modified_header,
+                parent,
+                false
+            )
+            return DateModifiedHeaderViewHolder(layoutItemBinding)
+        }
+    }
+}
--- app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/recyclerview/DeviceImagesDisplayRecyclerViewAdapter.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/recyclerview/DeviceImagesDisplayRecyclerViewAdapter.kt
@@ -1,12 +1,35 @@
 package com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images.recyclerview
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images.ImagesDisplayModel
 
+enum class DeviceImagesDisplayViewHolderType(val type: Int){
+    IMAGE(1),
+    GROUP_HEADER(2)
+}
+
 class DeviceImagesDisplayRecyclerViewAdapter :
     ListAdapter<ImagesDisplayModel, RecyclerView.ViewHolder>(DeviceImagesDisplayRecyclerViewDiffUtil) {
+
+    override fun getItemViewType(position: Int): Int {
+        return when(getItem(position)){
+            is ImagesDisplayModel.DeviceImageDisplay -> DeviceImagesDisplayViewHolderType.IMAGE.type
+            is ImagesDisplayModel.ImagesDateModifiedHeader -> DeviceImagesDisplayViewHolderType.GROUP_HEADER.type
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when(viewType){
+          DeviceImagesDisplayViewHolderType.IMAGE.type ->
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 
 }
