bb64ab1f6a01db91855ecd8b4f82dc944e4f82cd
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-19 23:41:30 +0100

    Building DeviceImagesDisplayRecyclerViewAdapter.kt

--- app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/ImagesDisplayModel.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/ImagesDisplayModel.kt
@@ -3,7 +3,14 @@ package com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images
 import com.salesground.zipbolt.model.DataToTransfer
 
 sealed class ImagesDisplayModel {
-    data class ImagesDateModifiedHeader(val dateModified: String) : ImagesDisplayModel()
-    data class DeviceImageDisplay(val deviceImage: DataToTransfer.DeviceImage) : ImagesDisplayModel()
+    abstract val id : String
+    data class ImagesDateModifiedHeader(val dateModified: String) : ImagesDisplayModel(){
+        override val id: String
+            get() = dateModified
+    }
+    data class DeviceImageDisplay(val deviceImage: DataToTransfer.DeviceImage) : ImagesDisplayModel(){
+        override val id: String
+            get() = deviceImage.imageUri.toString()
+    }
 }
 
--- app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/recyclerview/DeviceImagesDisplayRecyclerViewAdapter.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/recyclerview/DeviceImagesDisplayRecyclerViewAdapter.kt
@@ -1,4 +1,28 @@
 package com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images.recyclerview
 
-class DeviceImagesDisplayRecyclerViewAdapter {
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images.ImagesDisplayModel
+
+class DeviceImagesDisplayRecyclerViewAdapter :
+    ListAdapter<ImagesDisplayModel, RecyclerView.ViewHolder>(DeviceImagesDisplayRecyclerViewDiffUtil) {
+
+
+}
+
+object DeviceImagesDisplayRecyclerViewDiffUtil : DiffUtil.ItemCallback<ImagesDisplayModel>(){
+    override fun areItemsTheSame(
+        oldItem: ImagesDisplayModel,
+        newItem: ImagesDisplayModel
+    ): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: ImagesDisplayModel,
+        newItem: ImagesDisplayModel
+    ): Boolean {
+       return oldItem == newItem
+    }
 }
