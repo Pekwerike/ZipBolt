38f3752644ed11697f7843f3a8e599e36f9f77be
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-20 00:24:01 +0100

    Refactored ImagesDisplayModel.kt

--- app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/ImagesDisplayModel.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/categorycontentsdisplay/images/ImagesDisplayModel.kt
@@ -2,15 +2,12 @@ package com.salesground.zipbolt.ui.screen.categorycontentsdisplay.images
 
 import com.salesground.zipbolt.model.DataToTransfer
 
-sealed class ImagesDisplayModel {
-    abstract val id : String
-    data class ImagesDateModifiedHeader(val dateModified: String) : ImagesDisplayModel(){
-        override val id: String
-            get() = dateModified
-    }
-    data class DeviceImageDisplay(val deviceImage: DataToTransfer.DeviceImage) : ImagesDisplayModel(){
-        override val id: String
-            get() = deviceImage.imageUri.toString()
-    }
+sealed class ImagesDisplayModel(val id : String) {
+
+    data class ImagesDateModifiedHeader(val dateModified: String)
+        : ImagesDisplayModel(dateModified)
+
+    data class DeviceImageDisplay(val deviceImage: DataToTransfer.DeviceImage)
+        : ImagesDisplayModel(deviceImage.imageUri.toString())
 }
 
