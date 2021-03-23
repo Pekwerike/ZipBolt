e1c0b6e1737159149755330c46279b15c2f31d40
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-23 10:55:33 +0100

    Building all_media_on_device.xml

--- app/src/main/java/com/salesground/zipbolt/ui/screen/allmedia/AllMediaOnDevice.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/allmedia/AllMediaOnDevice.kt
@@ -1,2 +1,11 @@
 package com.salesground.zipbolt.ui.screen.allmedia
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+
+@Composable
+fun AllMediaOnDevice(){
+    AndroidViewBinding() {
+        
+    }
+}
