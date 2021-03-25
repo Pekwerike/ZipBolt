a747018dce4b7c076d26fb94b63e53e1034e38b0
Prosper Ekwerike <pekwerike@gmail.com>
2021-03-25 23:36:25 +0100

    Building new UI flow and entry point for the application

--- app/src/main/java/com/salesground/zipbolt/ui/screen/UIEntryPoint.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/screen/UIEntryPoint.kt
@@ -1,35 +1,68 @@
 package com.salesground.zipbolt.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collection.mutableVectorOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.salesground.zipbolt.databinding.ZipBoltEntryPointLayoutBinding
 
 @ExperimentalMaterialApi
 @Composable
 fun UIEntryPoint() {
 
     ModalBottomSheetLayout(sheetContent = {
-
+        // TODO Display the various categories for connection
     }) {
+        AndroidViewBinding(ZipBoltEntryPointLayoutBinding::inflate) {
+            zipBoltEntryPointComposeView.setContent {
+                Scaffold(
+                    floatingActionButton = {
 
+                    }
+                ) {
+                    // Place navHost here
+                }
+            }
+
+            zipBoltPersistentBottomSheetComposeView.setContent {
+                // place different bottom sheet contents here
+            }
+        }
     }
 }
 
+
+enum class ConnectionCategory(
+    val actionLabel: String,
+    val categoryLogo: ImageVector? = null
+) {
+    ANDROID(actionLabel = "Connect to Android device"),
+    IPHONE(actionLabel = "Connect to Iphone"),
+    DESKTOP(actionLabel = "Connect to Desktop"),
+    SHARE_ZIP_BOLT(actionLabel = "Share ZipBolt")
+}
+
 @Composable
-fun ModalBottomSheetConnectionAction(
+fun ModalBottomSheetConnectionCategory(
     platformLogo: ImageVector, platformLogoContentDescription: String,
-    actionLabel: String
+    actionLabel: String, onConnectionCategoryClicked: (String) -> Unit
 ) {
     Row(
         modifier = Modifier
-            .padding(8.dp)
+            .padding(4.dp)
+            .clickable {
+                onConnectionCategoryClicked(actionLabel)
+            }
+            .padding(4.dp)
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
