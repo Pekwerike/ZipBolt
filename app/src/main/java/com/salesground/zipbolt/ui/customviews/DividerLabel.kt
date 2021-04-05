848deda58feee7bb150ba42e9e7c3f05fa74a64d
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-05 21:55:03 +0100

    DividerLabel stroke height can now be configured from xml

--- app/src/main/java/com/salesground/zipbolt/ui/customviews/DividerLabel.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/customviews/DividerLabel.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import com.google.android.material.textview.MaterialTextView
+import com.salesground.zipbolt.R
 
 class DividerLabel @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -15,38 +16,55 @@ class DividerLabel @JvmOverloads constructor(
     private val dividerPaint: Paint
 
     init {
-        textAlignment = TEXT_ALIGNMENT_CENTER
-        dividerPaint = Paint().apply {
-            style = Paint.Style.STROKE
-            isAntiAlias = true
-            isDither = true
-            color = textColors.defaultColor
-            strokeWidth = 1f * context.resources.displayMetrics.scaledDensity
-            strokeCap = Paint.Cap.ROUND
-            strokeJoin = Paint.Join.ROUND
+        context.theme.obtainStyledAttributes(R.styleable.DividerLabel).apply {
+            dividerPaint = Paint().apply {
+                style = Paint.Style.STROKE
+                isAntiAlias = true
+                isDither = true
+                color = textColors.defaultColor
+                strokeWidth = getFloat(R.styleable.DividerLabel_strokeLineHeight, 1f)* context.resources.displayMetrics.scaledDensity
+                strokeCap = Paint.Cap.ROUND
+                strokeJoin = Paint.Join.ROUND
+            }
+            recycle()
         }
+        textAlignment = TEXT_ALIGNMENT_CENTER
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        Log.i("TextSize", textSize.toString())
         canvas?.let {
-            drawDividerLine(it)
+           val size = ((textSize / context.resources.displayMetrics.density)
+                    * length())
+            drawDividerLine(
+                canvas = it,
+                viewHeight = measuredHeight,
+                viewWidth = measuredWidth,
+                writtenTextSize = size
+            )
         }
     }
 
-    private fun drawDividerLine(canvas: Canvas) {
+    private fun drawDividerLine(
+        canvas: Canvas,
+        viewHeight: Int, viewWidth: Int,
+        writtenTextSize: Float,
+    ) {
+
         canvas.drawLine(
             paddingLeft.toFloat(),
-            measuredHeight * 0.5f, (measuredWidth * 0.5f) - textSize,
-            measuredHeight * 0.5f,
+            viewHeight * 0.5f,
+            (viewWidth * 0.5f) - writtenTextSize,
+            viewHeight * 0.5f,
             dividerPaint
         )
         canvas.drawLine(
-            measuredWidth * 0.50f + textSize,
-            measuredHeight * 0.5f, (measuredWidth - paddingRight).toFloat(),
-            measuredHeight * 0.5f,
+            (viewWidth * 0.5f) + writtenTextSize,
+            viewHeight * 0.5f, (viewWidth - paddingRight).toFloat(),
+            viewHeight * 0.5f,
             dividerPaint
         )
     }
+
 }
