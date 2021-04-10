42baf69d8175e4aee12613d416bfd416bc462d05
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-10 10:10:36 +0100

    Integrated PeerConnectionState into th MainActivityViewModel

--- app/src/main/java/com/salesground/zipbolt/model/ui/PeerConnectionState.kt
+++ app/src/main/java/com/salesground/zipbolt/model/ui/PeerConnectionState.kt
@@ -1,19 +1,26 @@
 package com.salesground.zipbolt.model.ui
 
 import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pInfo
 import com.salesground.zipbolt.model.DataToTransfer
 
-sealed class PeerConnectionState
+/*
+"Created PeerConnectionState to represent the various states of the
+ persistent bottom sheet layout during peer discovery and connection"
+*/
+sealed class PeerConnectionState {
 
-object NoConnectionAction : PeerConnectionState()
+    object NoConnectionAction : PeerConnectionState()
 
-data class CollapsedSearchingForPeer(val numberOfDevicesFound: Int) : PeerConnectionState()
+    data class CollapsedSearchingForPeer(val numberOfDevicesFound: Int) : PeerConnectionState()
 
-data class ExpandedSearchingForPeer(val devices: MutableList<WifiP2pDevice>) : PeerConnectionState()
+    data class ExpandedSearchingForPeer(val devices: MutableList<WifiP2pDevice>) :
+        PeerConnectionState()
 
-data class CollapsedConnectedToPeer(
-    val peeredDevice: WifiP2pDevice,
-    val currentFileTransfer: DataToTransfer
-) : PeerConnectionState()
+    data class CollapsedConnectedToPeer(
+        val peeredDeviceConnectionInfo: WifiP2pInfo
+    ) : PeerConnectionState()
 
-data class ExpandeedConnectedToPeer(val peeredDevice : WifiP2pDevice) : PeerConnectionState()
+    data class ExpandedConnectedToPeer(val peeredDeviceConnectionInfo: WifiP2pInfo) :
+        PeerConnectionState()
+}
--- app/src/main/java/com/salesground/zipbolt/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/salesground/zipbolt/viewmodel/MainActivityViewModel.kt
@@ -8,8 +8,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.salesground.zipbolt.foregroundservice.ClientService
 import com.salesground.zipbolt.foregroundservice.ServerService
+import com.salesground.zipbolt.model.ui.PeerConnectionState
 
 class MainActivityViewModel : ViewModel() {
+    private var _peerConnectionState =
+        MutableLiveData<PeerConnectionState>(PeerConnectionState.NoConnectionAction)
+    val peerConnectionState: LiveData<PeerConnectionState> = _peerConnectionState
+
+
     private var _isWifiP2pEnabled = MutableLiveData(false)
     val isWifiP2pEnabled: LiveData<Boolean> = _isWifiP2pEnabled
 
@@ -26,10 +32,14 @@ class MainActivityViewModel : ViewModel() {
         private set
 
     private var _clientService = MutableLiveData<ClientService>(null)
-    val clientService : LiveData<ClientService> = _clientService
+    val clientService: LiveData<ClientService> = _clientService
 
     private var _serverService = MutableLiveData<ServerService>(null)
-    val serverService : LiveData<ServerService> = _serverService
+    val serverService: LiveData<ServerService> = _serverService
+
+    fun updatePeerConnectionState(peerConnectionState: PeerConnectionState) {
+        _peerConnectionState.value = peerConnectionState
+    }
 
     fun serverServiceReady(serverService: ServerService) {
         _serverService.value = serverService
@@ -46,11 +56,48 @@ class MainActivityViewModel : ViewModel() {
 
     fun discoveredPeersListChanged(newDiscoveredPeersList: MutableList<WifiP2pDevice>) {
         discoveredPeersListState.value = newDiscoveredPeersList
+
+        when (_peerConnectionState.value) {
+            is PeerConnectionState.CollapsedSearchingForPeer -> {
+                _peerConnectionState.value = PeerConnectionState.CollapsedSearchingForPeer(
+                    numberOfDevicesFound =
+                    newDiscoveredPeersList.size
+                )
+            }
+            is PeerConnectionState.ExpandedSearchingForPeer -> {
+                _peerConnectionState.value = PeerConnectionState.ExpandedSearchingForPeer(
+                    devices = newDiscoveredPeersList
+                )
+            }
+        }
     }
 
     fun peeredDeviceConnectionInfoUpdated(connectionInfo: WifiP2pInfo) {
         _peeredDeviceConnectionInfo.value = connectionInfo
         peeredDeviceConnectionInfoState.value = connectionInfo
+
+        when (_peerConnectionState.value) {
+            is PeerConnectionState.CollapsedConnectedToPeer -> {
+                _peerConnectionState.value = PeerConnectionState.CollapsedConnectedToPeer(
+                    peeredDeviceConnectionInfo = connectionInfo
+                )
+            }
+            is PeerConnectionState.ExpandedConnectedToPeer -> {
+                _peerConnectionState.value = PeerConnectionState.ExpandedConnectedToPeer(
+                    peeredDeviceConnectionInfo = connectionInfo
+                )
+            }
+            is PeerConnectionState.ExpandedSearchingForPeer -> {
+                _peerConnectionState.value = PeerConnectionState.ExpandedConnectedToPeer(
+                    peeredDeviceConnectionInfo = connectionInfo
+                )
+            }
+            else -> {
+                _peerConnectionState.value = PeerConnectionState.CollapsedConnectedToPeer(
+                    peeredDeviceConnectionInfo = connectionInfo
+                )
+            }
+        }
     }
 
 }
