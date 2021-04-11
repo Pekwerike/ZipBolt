393aa8deb3e132ac955f2660b3d78b40368ef17f
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-11 15:42:09 +0100

    Refactoring SearchingForDevicesAnimatedView

--- app/src/main/java/com/salesground/zipbolt/ui/customviews/SearchingForDevicesAnimatedView.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/customviews/SearchingForDevicesAnimatedView.kt
@@ -6,27 +6,30 @@ import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
-import androidx.compose.ui.graphics.Color
 import androidx.core.content.ContextCompat
 import com.salesground.zipbolt.R
 import kotlin.math.max
-import kotlin.math.min
 import kotlin.math.roundToInt
 
 class SearchingForDevicesAnimatedView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    var numberOfOuterCircles = 4
-    val radiusIncrement : Float = 1 / numberOfOuterCircles.toFloat()
-    var center = PointF(0f, 0f)
+    private var numberOfOuterCircles = 4
+    private val radiusIncrement: Float = 1 / numberOfOuterCircles.toFloat()
+    private var center = PointF(0f, 0f)
+
     private val personDrawable: Drawable = ContextCompat
         .getDrawable(context, R.drawable.person_icon)!!.apply {
             setTint(ContextCompat.getColor(context, R.color.white))
         }
+    private var drawableLeft: Int = 0
+    private var drawableTop: Int = 0
 
-    var radius = 20f * resources.displayMetrics.density
-    var baseRadius: Float = 0f
+    private var externalRadius = 20f * resources.displayMetrics.density
+    private var baseRadius: Float = 0f
+    private var coreCircleRadius: Float = 0f
+
 
     private val circlePaint = Paint().apply {
         style = Paint.Style.FILL
@@ -35,12 +38,17 @@ class SearchingForDevicesAnimatedView @JvmOverloads constructor(
         isAntiAlias = true
         alpha = 20
     }
-    private val coreCirclePaint = Paint().apply{
+
+    private val coreCirclePaint = Paint().apply {
         style = Paint.Style.FILL
-       // color = ContextCompat.getColor(context, R.color.blue_415)
+        //color = ContextCompat.getColor(context, R.color.blue_415)
         isDither = true
-        shader = LinearGradient(0f, 0f, 100f, 0f, intArrayOf(ContextCompat.getColor(context, R.color.blue_415),
-        ContextCompat.getColor(context, R.color.purple_200)), null, Shader.TileMode.MIRROR)
+        shader = LinearGradient(
+            0f, 0f, 100f, 0f, intArrayOf(
+                ContextCompat.getColor(context, R.color.blue_415),
+                ContextCompat.getColor(context, R.color.purple_200)
+            ), null, Shader.TileMode.MIRROR
+        )
         isAntiAlias = true
         alpha = 80
     }
@@ -55,12 +63,9 @@ class SearchingForDevicesAnimatedView @JvmOverloads constructor(
 
     private fun drawCore(canvas: Canvas) {
 
-        // draw base circle
-        canvas.drawCircle(center.x, center.y, baseRadius + (baseRadius * 0.2f), coreCirclePaint)
+        // draw core circle
+        canvas.drawCircle(center.x, center.y, coreCircleRadius, coreCirclePaint)
 
-        // draw person drawable
-        val drawableLeft = (center.x - baseRadius).roundToInt()
-        val drawableTop = (center.y - baseRadius).roundToInt()
         personDrawable.setBounds(
             drawableLeft,
             drawableTop,
@@ -72,11 +77,11 @@ class SearchingForDevicesAnimatedView @JvmOverloads constructor(
     }
 
     private fun drawCircles(canvas: Canvas) {
-        var currentRadius : Float  = 0f
-        for(i in 1 .. 4){
+        var currentRadius: Float = 0f
+        for (i in 1..4) {
             currentRadius += radiusIncrement
             canvas.drawCircle(
-                center.x, center.y,  max(baseRadius  + (baseRadius * 0.2f), radius * currentRadius),
+                center.x, center.y, max(coreCircleRadius, externalRadius * currentRadius),
                 circlePaint
             )
         }
@@ -86,14 +91,18 @@ class SearchingForDevicesAnimatedView @JvmOverloads constructor(
         super.onSizeChanged(w, h, oldw, oldh)
         center = PointF(w * 0.5f, h * 0.5f)
         baseRadius = w * 0.10f
+        coreCircleRadius = baseRadius + (baseRadius * 0.2f)
+        // draw person drawable
+          drawableLeft = (center.x - baseRadius).roundToInt()
+        drawableTop = (center.y - baseRadius).roundToInt()
 
-        ValueAnimator.ofFloat(baseRadius  + (baseRadius * 0.2f), w * 0.45f).apply {
+        ValueAnimator.ofFloat(coreCircleRadius, w * 0.45f).apply {
             repeatMode = ValueAnimator.REVERSE
             repeatCount = ValueAnimator.INFINITE
             duration = 1000
             start()
         }.addUpdateListener {
-            radius = it.animatedValue as Float
+            externalRadius = it.animatedValue as Float
             invalidate()
         }
     }
