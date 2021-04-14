375951747448f8dc801b46df29d9e46b5cc8cd7b
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-14 14:38:38 +0100

    Creating header for DiscoveredPeersRecyclerViewAdapter

--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedsearchingforpeersinformation/DiscoveredPeerViewHolder.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedsearchingforpeersinformation/DiscoveredPeerViewHolder.kt
@@ -36,5 +36,4 @@ class DiscoveredPeerViewHolder(
             return DiscoveredPeerViewHolder(layoutBinding)
         }
     }
-
 }
--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedsearchingforpeersinformation/DiscoveredPeersRecyclerViewAdapter.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedsearchingforpeersinformation/DiscoveredPeersRecyclerViewAdapter.kt
@@ -4,30 +4,36 @@ import android.net.wifi.p2p.WifiP2pDevice
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 class DiscoveredPeersRecyclerViewAdapter(
     private val connectToDeviceClickListener : ConnectToDeviceClickListener
 ) :
-    ListAdapter<WifiP2pDevice, DiscoveredPeerViewHolder>(DiscoveredPeersRecyclerViewAdapterDiffUitl) {
+    ListAdapter<DiscoveredPeersDataItem, RecyclerView.ViewHolder>(DiscoveredPeersRecyclerViewAdapterDiffUitl) {
 
     interface ConnectToDeviceClickListener{
         fun onConnectToDevice(wifiP2pDevice: WifiP2pDevice)
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiscoveredPeerViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return DiscoveredPeerViewHolder.createDiscoveredPeerViewHolder(parent)
     }
 
-    override fun onBindViewHolder(holder: DiscoveredPeerViewHolder, position: Int) {
-        holder.bindDeviceData(getItem(position), connectToDeviceClickListener)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
     }
 
-    object DiscoveredPeersRecyclerViewAdapterDiffUitl : DiffUtil.ItemCallback<WifiP2pDevice>() {
-        override fun areItemsTheSame(oldItem: WifiP2pDevice, newItem: WifiP2pDevice): Boolean {
-            return oldItem.deviceName == newItem.deviceName
+    object DiscoveredPeersRecyclerViewAdapterDiffUitl : DiffUtil.ItemCallback<DiscoveredPeersDataItem>() {
+        override fun areItemsTheSame(oldItem: DiscoveredPeersDataItem, newItem: DiscoveredPeersDataItem): Boolean {
+            return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: WifiP2pDevice, newItem: WifiP2pDevice): Boolean {
+        override fun areContentsTheSame(oldItem: DiscoveredPeersDataItem, newItem: DiscoveredPeersDataItem): Boolean {
             return oldItem == newItem
         }
     }
+}
+
+sealed class DiscoveredPeersDataItem(val id : String){
+    object Header : DiscoveredPeersDataItem(id = "discoveredPeersDataItemHeader")
+    class  DiscoveredPeer(wifiP2pDevice: WifiP2pDevice) : DiscoveredPeersDataItem(id = wifiP2pDevice.deviceName)
 }
