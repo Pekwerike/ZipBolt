78860b722afe2f5cc8d66743b855b909424a4dab
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-15 21:19:46 +0100

    Building ZipBoltMediaTransferProtocol to replace ZipBoltMTP

--- app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
+++ app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
@@ -2,6 +2,8 @@ package com.salesground.zipbolt.communicationprotocol
 
 import javax.inject.Inject
 
-class ZipBoltMediaTransferProtocol @Inject constructor(): MediaTransferProtocol {
+class ZipBoltMediaTransferProtocol @Inject constructor(
+    private val mediaTransferProtocol: MediaTransferProtocol
+) : MediaTransferProtocol {
 
 }
