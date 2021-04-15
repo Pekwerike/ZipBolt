7a5c213c8d1bf662419fa4f229310ca9852518c6
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-15 22:11:24 +0100

    Finished the implementation of transferMedia function in ZipBoltMediaTransferProtocol

--- app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
+++ app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
@@ -1,16 +1,18 @@
 package com.salesground.zipbolt.communicationprotocol
 
 import android.content.Context
+import android.os.ParcelFileDescriptor
 import com.salesground.zipbolt.model.DataToTransfer
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.DataInputStream
 import java.io.DataOutputStream
+import java.io.FileInputStream
 import javax.inject.Inject
 
 class ZipBoltMediaTransferProtocol @Inject constructor(
-    @ApplicationContext private val context : Context,
+    @ApplicationContext private val context: Context,
     private val mediaTransferProtocol: MediaTransferProtocol
 ) : MediaTransferProtocol {
 
@@ -24,7 +26,19 @@ class ZipBoltMediaTransferProtocol @Inject constructor(
             dataOutputStream.writeLong(dataToTransfer.dataSize)
             dataOutputStream.writeUTF(dataToTransfer.dataType)
 
+            context.contentResolver.openFileDescriptor(dataToTransfer.dataUri, "r")
+                ?.also { parcelFileDescriptor: ParcelFileDescriptor ->
+                    val dataFileInputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+                    val bufferArray = ByteArray(10_000_000)
+                    var lengthOfDataRead: Int
 
+                    while (dataFileInputStream.read(bufferArray)
+                            .also { lengthOfDataRead = it } > 0
+                    ) {
+                        dataOutputStream.write(bufferArray, 0, lengthOfDataRead)
+                    }
+                    dataFileInputStream.close()
+                }
         }
     }
 
