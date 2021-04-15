a2bc43b59c51dc16bfecc730cbb0a7345ff865f8
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-15 22:06:48 +0100

    Implementing trasferMedia function in ZipBoltMediaTransferProtocol

--- app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
+++ app/src/main/java/com/salesground/zipbolt/communicationprotocol/ZipBoltMediaTransferProtocol.kt
@@ -1,19 +1,31 @@
 package com.salesground.zipbolt.communicationprotocol
 
+import android.content.Context
 import com.salesground.zipbolt.model.DataToTransfer
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import javax.inject.Inject
 
 class ZipBoltMediaTransferProtocol @Inject constructor(
+    @ApplicationContext private val context : Context,
     private val mediaTransferProtocol: MediaTransferProtocol
 ) : MediaTransferProtocol {
 
+    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun transferMedia(
         dataToTransfer: DataToTransfer,
         dataOutputStream: DataOutputStream
     ) {
-        dataOutputStream.writeUTF(dataToTransfer.)
+        withContext(Dispatchers.IO) {
+            dataOutputStream.writeUTF(dataToTransfer.dataDisplayName)
+            dataOutputStream.writeLong(dataToTransfer.dataSize)
+            dataOutputStream.writeUTF(dataToTransfer.dataType)
+
+
+        }
     }
 
     override suspend fun receiveMedia(dataInputStream: DataInputStream) {
