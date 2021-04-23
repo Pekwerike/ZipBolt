0456eb1d0436ba8915c61d1d1597517cfff58f51
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-23 22:24:42 +0100

    Building expanded_connected_to_peer_no_action.xml

--- app/src/main/java/com/salesground/zipbolt/MainActivity.kt
+++ app/src/main/java/com/salesground/zipbolt/MainActivity.kt
@@ -165,7 +165,7 @@ class MainActivity : AppCompatActivity() {
 
 
         inflate(layoutInflater).apply {
-            setContentView(CollapsedConnectedToPeerNoActionBinding.inflate(layoutInflater).root)
+            setContentView(ExpandedConnectedToPeerNoActionBinding.inflate(layoutInflater).root)
             activityMainBinding = this
             connectToPeerButton.setOnClickListener {
                 if (it.alpha > 0f) {
