3fef578483b1ccba10571e9488c391ba7e2c8cff
Prosper Ekwerike <pekwerike@gmail.com>
2021-05-06 16:19:37 +0100

    Building OngoingDataTransferViewHolder

--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedconnectedtopeertransferongoing/OngoingDataTransferViewHolder.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/expandedconnectedtopeertransferongoing/OngoingDataTransferViewHolder.kt
@@ -1,4 +1,30 @@
 package com.salesground.zipbolt.ui.recyclerview.expandedconnectedtopeertransferongoing
 
-class OngoingDataTransferViewHolder {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.databinding.OngoingDataTransferLayoutItemBinding
+
+class OngoingDataTransferViewHolder(private val ongoingDataTransferLayoutItemBinding: OngoingDataTransferLayoutItemBinding) :
+    RecyclerView.ViewHolder(ongoingDataTransferLayoutItemBinding.root) {
+
+    fun bindData(){
+        ongoingDataTransferLayoutItemBinding.apply {
+            dataDisplayName = "Replace oo"
+            dataPercentState = 908
+        }
+    }
+    companion object {
+        fun createViewHolder(parent: ViewGroup): OngoingDataTransferViewHolder {
+            val layoutItemBinding = DataBindingUtil.inflate<OngoingDataTransferLayoutItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.ongoing_data_transfer_layout_item,
+                parent,
+                false
+            )
+            return OngoingDataTransferViewHolder(layoutItemBinding)
+        }
+    }
 }
