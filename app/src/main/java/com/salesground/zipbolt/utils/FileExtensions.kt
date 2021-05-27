c39d3dc645da40ca237b45acb46f2f879608a6d6
Prosper C Ekwerike <Prosper.Ekwerike@ng.ey.com>
2021-05-27 12:39:30 +0100

    Wrote local unit test for FileExtensions.kt

--- app/src/main/java/com/salesground/zipbolt/utils/FileExtensions.kt
+++ app/src/main/java/com/salesground/zipbolt/utils/FileExtensions.kt
@@ -3,6 +3,7 @@ package com.salesground.zipbolt.utils
 import android.content.Context
 import android.media.MediaMetadataRetriever
 import android.net.Uri
+import java.math.MathContext
 
 fun Uri.getMediaDuration(context: Context): String {
     var mediaDuration: String? = ""
@@ -21,11 +22,14 @@ fun Uri.getMediaDuration(context: Context): String {
 }
 
 fun Long.transformDataSizeToMeasuredUnit(): String {
-    return when {
-        this in 1..99 -> "${this}b"
-        this in 1000..999_999 -> "${this / 1000}kb"
-        this in 1_000_000..999_999_999 -> "${this / 1_000_000}mb"
-        this in 1_000_000_000..999_999_999_999 -> "${this / 1_000_000_000}gb"
-        else -> "${this / 1_000_000_000_000}tb"
+    return when (this) {
+        in 1..99 -> "${this}b"
+        in 1000..999_999 -> "${(this / 1000F).toBigDecimal().round(MathContext(3))}kb"
+        in 1_000_000..9_999_999 -> "${(this / 1_000_000F).toBigDecimal().round(MathContext(2))}mb"
+        in 10_000_000..999_999_999 -> "${(this / 1_000_000F).toBigDecimal().round(MathContext(3))}mb"
+        in 1_000_000_000..999_999_999_999 -> "${
+            (this / 1_000_000_000F).toBigDecimal().round(MathContext(3))
+        }gb"
+        else -> "${this / 1_000_000_000_000F}tb"
     }
 }
