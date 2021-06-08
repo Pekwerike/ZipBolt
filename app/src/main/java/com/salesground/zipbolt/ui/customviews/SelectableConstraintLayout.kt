ff2e15153f1b43e5a62ca7aca84b786a2120309d
Prosper C Ekwerike <Prosper.Ekwerike@ng.ey.com>
2021-06-08 13:40:53 +0100

    Created SelectableConstraintLayout

---
+++ app/src/main/java/com/salesground/zipbolt/ui/customviews/SelectableConstraintLayout.kt
@@ -0,0 +1,83 @@
+package com.salesground.zipbolt.ui.customviews
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.CornerPathEffect
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.salesground.zipbolt.R
+
+class SelectableConstraintLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+    private var isViewSelected = false
+    private val cornerRect = RectF()
+    private val cornerRectRadius = 4 * resources.displayMetrics.density
+    private val cornerRectStrokePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        isAntiAlias = true
+        isDither = true
+        color = ContextCompat.getColor(context, R.color.blue_415)
+        strokeWidth = 4 * resources.displayMetrics.density
+        pathEffect = CornerPathEffect(cornerRectRadius)
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    private val cornerRectFillPaint = Paint().apply {
+        style = Paint.Style.FILL
+        isAntiAlias = true
+        isDither = true
+        color = ContextCompat.getColor(context, R.color.blue_415_15_percent_alpha)
+
+    }
+
+    override fun dispatchDraw(canvas: Canvas?) {
+        super.dispatchDraw(canvas)
+        if (isViewSelected) {
+            canvas?.let {
+                drawScrim(it)
+                drawRoundedCorners(it)
+            }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        cornerRect.apply {
+            left = 0f
+            top = 0f
+            right = measuredWidth.toFloat()
+            bottom = measuredHeight.toFloat()
+        }
+    }
+
+    private fun drawScrim(canvas: Canvas) {
+        canvas.drawRect(
+            cornerRect,
+            cornerRectFillPaint
+        )
+    }
+
+    private fun drawRoundedCorners(canvas: Canvas) {
+        canvas.drawRect(
+            cornerRect,
+            cornerRectStrokePaint
+        )
+    }
+
+    fun setIsViewSelected(selected: Boolean) {
+        if (isViewSelected && !selected) {
+            isViewSelected = selected
+            invalidate()
+        } else if (!isViewSelected && selected) {
+            isViewSelected = selected
+            invalidate()
+        }
+    }
+
+}
