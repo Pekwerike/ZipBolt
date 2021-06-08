c350d3d5262f4bc80f18acd77929b23f5e6195f8
Prosper C Ekwerike <Prosper.Ekwerike@ng.ey.com>
2021-06-08 12:41:52 +0100

    Removed color animation in SelectableLinearLayout

--- app/src/main/java/com/salesground/zipbolt/ui/customviews/SelectableLinearLayout.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/customviews/SelectableLinearLayout.kt
@@ -10,6 +10,7 @@ import android.graphics.RectF
 import android.util.AttributeSet
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.animation.AnimatorSetCompat
 import com.salesground.zipbolt.R
 
 class SelectableLinearLayout @JvmOverloads constructor(
@@ -17,18 +18,13 @@ class SelectableLinearLayout @JvmOverloads constructor(
 ) : LinearLayoutCompat(context, attrs) {
     private var isViewSelected = false
     private val cornerRect = RectF()
-    private val colorLightBlue = ContextCompat.getColor(context, R.color.blue_415)
-    private val colorLightBlueTransparent =
-        ContextCompat.getColor(context, R.color.blue_415_15_percent_alpha)
-    private val colorTransparent = ContextCompat.getColor(context, android.R.color.transparent)
-
     private val cornerRectRadius = 4 * resources.displayMetrics.density
     private val cornerRectStrokePaint = Paint().apply {
         style = Paint.Style.STROKE
         isAntiAlias = true
         isDither = true
+        color = ContextCompat.getColor(context, R.color.blue_415)
         strokeWidth = 4 * resources.displayMetrics.density
-        color = colorTransparent
         pathEffect = CornerPathEffect(cornerRectRadius)
         strokeJoin = Paint.Join.ROUND
         strokeCap = Paint.Cap.ROUND
@@ -37,8 +33,8 @@ class SelectableLinearLayout @JvmOverloads constructor(
     private val cornerRectFillPaint = Paint().apply {
         style = Paint.Style.FILL
         isAntiAlias = true
-        color = colorTransparent
         isDither = true
+        color = ContextCompat.getColor(context, R.color.blue_415_15_percent_alpha)
 
     }
 
@@ -78,55 +74,12 @@ class SelectableLinearLayout @JvmOverloads constructor(
 
     fun setIsViewSelected(selected: Boolean) {
         if (isViewSelected && !selected) {
-
-            val strokeColorAnimator = ValueAnimator.ofInt(colorLightBlue, colorTransparent).apply {
-                duration = 500
-                addUpdateListener {
-                    val animatedColorValue = it.animatedValue as Int
-                    cornerRectStrokePaint.color = animatedColorValue
-                    invalidate()
-                }
-            }
-
-            val fillColorAnimator =
-                ValueAnimator.ofInt(colorLightBlueTransparent, colorTransparent).apply {
-                    duration = 500
-                    addUpdateListener {
-                        val animatedColorValue = it.animatedValue as Int
-                        cornerRectFillPaint.color = animatedColorValue
-                    }
-                }
-
-            AnimatorSet().run {
-                playTogether(strokeColorAnimator, fillColorAnimator)
-                start()
-            }
             isViewSelected = selected
+            invalidate()
         } else if (!isViewSelected && selected) {
-
-            val strokeColorAnimator = ValueAnimator.ofInt(colorTransparent, colorLightBlue).apply {
-                duration = 600
-                addUpdateListener {
-                    val animatedColorValue = it.animatedValue as Int
-                    cornerRectStrokePaint.color = animatedColorValue
-                    invalidate()
-                }
-            }
-
-            val fillColorAnimator =
-                ValueAnimator.ofInt(colorTransparent, colorLightBlueTransparent).apply {
-                    duration = 600
-                    addUpdateListener {
-                        val animatedColorValue = it.animatedValue as Int
-                        cornerRectFillPaint.color = animatedColorValue
-                    }
-                }
-
-            AnimatorSet().run {
-                playTogether(strokeColorAnimator, fillColorAnimator)
-                start()
-            }
             isViewSelected = selected
+            invalidate()
         }
     }
+
 }
