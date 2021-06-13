ee6183ccc9cdf75f4eabb39d6d767e8c59b5d40d
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-13 22:23:05 +0100

    Created UpgradedWifiDirectBroadcastReceiver

---
+++ app/src/main/java/com/salesground/zipbolt/broadcast/UpgradedWifiDirectBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.salesground.zipbolt.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class UpgradedWifiDirectBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
