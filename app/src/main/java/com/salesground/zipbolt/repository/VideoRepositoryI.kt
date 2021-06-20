5339a8c4ac1ec458a319b30d3b6d4e0a8a850b3b
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-20 20:31:21 +0100

    Building new VideoRepository layer

---
+++ app/src/main/java/com/salesground/zipbolt/repository/VideoRepositoryI.kt
@@ -0,0 +1,19 @@
+package com.salesground.zipbolt.repository
+
+import com.salesground.zipbolt.communication.MediaTransferProtocol
+import com.salesground.zipbolt.model.DataToTransfer
+import java.io.DataInputStream
+
+interface VideoRepositoryI {
+
+    suspend fun insertVideoIntoMediaStore(
+        videoName: String,
+        videoSize: Long,
+        dataInputStream: DataInputStream,
+        transferMetaDataUpdateListener: MediaTransferProtocol.TransferMetaDataUpdateListener,
+        dataReceiveListener: MediaTransferProtocol.DataReceiveListener
+    )
+
+    suspend fun getMetaDataOfVideo(video: DataToTransfer.DeviceVideo): DataToTransfer
+    suspend fun getVideosOnDevice(limit: Int = 0): MutableList<DataToTransfer>
+}
