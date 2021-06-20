2d0bc1e45ea2e141cfe1c7adeb1a7eb9c9fac26e
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-20 20:27:16 +0100

    Removed ImageRepositoryInitial, ImageRepositoryInitialTest and ImageRepositoryInterface

--- app/src/main/java/com/salesground/zipbolt/repository/repositoryinterface/ImageRepositoryInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.salesground.zipbolt.repository.repositoryinterface
-
-import com.salesground.zipbolt.model.MediaModel
-import kotlinx.coroutines.flow.Flow
-import java.io.File
-
-interface ImageRepositoryInterface {
-    fun convertImageModelToFile(imagesToConvert: MutableList<MediaModel>): MutableList<File>
-}
