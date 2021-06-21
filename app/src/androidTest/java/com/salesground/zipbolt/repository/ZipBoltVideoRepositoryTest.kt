a2c00e90cfab075685be6fe06535ec5fedb645eb
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-21 11:15:16 +0100

    Writing ZipBoltVideoRepositoryTest

--- app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltVideoRepositoryTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltVideoRepositoryTest.kt
@@ -1,10 +1,27 @@
 package com.salesground.zipbolt.repository
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.salesground.zipbolt.repository.implementation.ZipBoltVideoRepository
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
 
+@HiltAndroidTest
 class ZipBoltVideoRepositoryTest {
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private lateinit var zipBoltVideoRepository : ZipBoltVideoRepository
+
+    @Before
+    fun setUp(){
+        zipBoltVideoRepository = ZipBoltVideoRepository(
+            ZipBoltSavedFilesRepository()
+        )
+    }
+
 
     @Test
     fun insertVideoIntoMediaStore() {
@@ -12,5 +29,8 @@ class ZipBoltVideoRepositoryTest {
 
     @Test
     fun getVideosOnDevice() {
+        runBlocking {
+            zipBoltVideoRepository.getVideosOnDevice()
+        }
     }
 }
