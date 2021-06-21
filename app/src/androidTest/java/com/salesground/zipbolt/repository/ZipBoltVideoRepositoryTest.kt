390f15e41991a30b60a3fc0925103e40a2e61293
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-21 11:46:24 +0100

    Completed insertVideoIntoMediaStore for ZipBoltVideoRepositoryTest

--- app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltVideoRepositoryTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/ZipBoltVideoRepositoryTest.kt
@@ -1,8 +1,11 @@
 package com.salesground.zipbolt.repository
 
 import android.content.Context
+import android.net.Uri
 import android.util.Log
 import androidx.test.core.app.ApplicationProvider
+import com.salesground.zipbolt.communication.MediaTransferProtocol
+import com.salesground.zipbolt.model.DataToTransfer
 import com.salesground.zipbolt.repository.implementation.ZipBoltVideoRepository
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.runBlocking
@@ -10,6 +13,8 @@ import org.junit.Assert.*
 import org.junit.Before
 
 import org.junit.Test
+import java.io.DataInputStream
+import java.io.FileInputStream
 
 @HiltAndroidTest
 class ZipBoltVideoRepositoryTest {
@@ -26,6 +31,47 @@ class ZipBoltVideoRepositoryTest {
 
     @Test
     fun insertVideoIntoMediaStore() {
+        runBlocking {
+            val videoToInsertIntoMediaStore =
+                zipBoltVideoRepository.getVideosOnDevice(context).first()
+
+
+            zipBoltVideoRepository.insertVideoIntoMediaStore(
+                context = context,
+                videoName = videoToInsertIntoMediaStore.dataDisplayName,
+                videoSize = videoToInsertIntoMediaStore.dataSize,
+                dataInputStream = DataInputStream(
+                    context.contentResolver.openInputStream(
+                        videoToInsertIntoMediaStore.dataUri
+                    )
+                ),
+                transferMetaDataUpdateListener = object :
+                    MediaTransferProtocol.TransferMetaDataUpdateListener {
+                    override fun onMetaTransferDataUpdate(mediaTransferProtocolMetaData: MediaTransferProtocol.MediaTransferProtocolMetaData) {
+
+                    }
+                },
+                dataReceiveListener = object : MediaTransferProtocol.DataReceiveListener {
+                    override fun onReceive(
+                        dataDisplayName: String,
+                        dataSize: Long,
+                        percentageOfDataRead: Float,
+                        dataType: Int,
+                        dataUri: Uri?,
+                        dataTransferStatus: DataToTransfer.TransferStatus
+                    ) {
+                        assertEquals(DataToTransfer.MediaType.VIDEO.value, dataType)
+                        if (dataTransferStatus == DataToTransfer.TransferStatus.RECEIVE_COMPLETE) {
+                            assertEquals(100f, percentageOfDataRead)
+                            assert(dataUri != null)
+                        }else if(dataTransferStatus == DataToTransfer.TransferStatus.RECEIVE_ONGOING ||
+                                dataTransferStatus == DataToTransfer.TransferStatus.RECEIVE_STARTED){
+                            assertEquals(null, dataUri)
+                        }
+                    }
+                }
+            )
+        }
     }
 
     @Test
