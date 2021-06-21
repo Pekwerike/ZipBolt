a313f3b2810c9bcba0751041325f6abf0d3ca237
Prosper Ekwerike <pekwerike@gmail.com>
2021-06-21 11:04:19 +0100

    Created ZipBoltVideosRepositoryTest

---
+++ app/src/androidTest/java/com/salesground/zipbolt/repository/implementation/ZipBoltVideosRepositoryTest.kt
@@ -0,0 +1,16 @@
+package com.salesground.zipbolt.repository.implementation
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ZipBoltVideosRepositoryTest {
+
+    @Test
+    fun insertVideoIntoMediaStore() {
+    }
+
+    @Test
+    fun getVideosOnDevice() {
+    }
+}
