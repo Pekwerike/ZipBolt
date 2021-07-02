e19f6ba345e43b6a1f91b4fd5f8a6f9a6ca49032
Prosper Ekwerike <pekwerike@gmail.com>
2021-07-02 22:52:26 +0100

    Created UpgradedWifiDirectBroadcastReceiver, testing next

--- app/src/main/java/com/salesground/zipbolt/broadcast/UpgradedWifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/salesground/zipbolt/broadcast/UpgradedWifiDirectBroadcastReceiver.kt
@@ -6,37 +6,56 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.NetworkInfo
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pGroup
+import android.net.wifi.p2p.WifiP2pInfo
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Build
 
-class UpgradedWifiDirectBroadcastReceiver(context: Context) : BroadcastReceiver() {
-    val connectivityManager =
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+class UpgradedWifiDirectBroadcastReceiver(
+    private val wifiDirectBroadcastReceiverCallback: WifiDirectBroadcastReceiver.WifiDirectBroadcastReceiverCallback,
+    private val connectivityManager: ConnectivityManager,
+    private val wifiP2pManager: WifiP2pManager,
+    private val wifiP2pChannel: WifiP2pManager.Channel
+) : BroadcastReceiver() {
+
+    interface WifiDirectBroadcastReceiverCallback {
+        fun wifiOn()
+        fun wifiOff()
+        fun peersListAvailable(peersList: MutableList<WifiP2pDevice>)
+        fun connectedToPeer(
+            wifiP2pInfo: WifiP2pInfo,
+            peeredDevice: WifiP2pDevice
+        )
+
+        fun wifiP2pDiscoveryStopped()
+        fun wifiP2pDiscoveryStarted()
+        fun disconnectedFromPeer()
+    }
 
     override fun onReceive(context: Context?, intent: Intent?) {
         intent?.let {
             when (intent.action) {
                 WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
-                    // check if we are already connected to a wifi p2p device
-                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                    if (isConnectedToPeerNetwork(intent)) {
+                        wifiP2pManager.requestConnectionInfo(wifiP2pChannel) { wifiP2pInfo ->
+                            if (wifiP2pInfo.groupFormed) {
+                                intent.getParcelableExtra<WifiP2pGroup>(WifiP2pManager.EXTRA_WIFI_P2P_GROUP)
+                                    ?.let { wifiP2pGroup ->
+                                        val connectedDevice: WifiP2pDevice =
+                                            if (wifiP2pInfo.isGroupOwner) {
+                                                wifiP2pGroup.clientList.first()
+                                            } else {
+                                                wifiP2pGroup.owner
+                                            }
 
-                        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-                            ?.let { networkCapabilities: NetworkCapabilities ->
-                                if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) &&
-                                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_WIFI_P2P)
-                                ) {
-                                    // request for connection info
-
-                                }
+                                        wifiDirectBroadcastReceiverCallback.connectedToPeer(
+                                            wifiP2pInfo,
+                                            connectedDevice
+                                        )
+                                    }
                             }
-                    } else {
-                        // use the NetworkInfo class to check if we are connected or not
-                        intent.getParcelableExtra<NetworkInfo>(WifiP2pManager.EXTRA_NETWORK_INFO)
-                            ?.let { networkInfo: NetworkInfo ->
-                                if (networkInfo.isConnected) {
-                                    // request for connection info
-                                }
-                            }
+                        }
                     }
                 }
                 else -> {
@@ -44,5 +63,22 @@ class UpgradedWifiDirectBroadcastReceiver(context: Context) : BroadcastReceiver(
                 }
             }
         }
+    }
+
+    private fun isConnectedToPeerNetwork(intent: Intent): Boolean {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                ?.let { networkCapabilities: NetworkCapabilities ->
+                    return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) &&
+                            networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_WIFI_P2P)
+                }
+
+        } else {
+            intent.getParcelableExtra<NetworkInfo>(WifiP2pManager.EXTRA_NETWORK_INFO)
+                ?.let { networkInfo: NetworkInfo ->
+                    return networkInfo.isConnected
+                }
+        }
+        return false
     }
 }
