dc29ce2366a1f0d6b77f5561827de43a6c6f68e1
Prosper Ekwerike <pekwerike@gmail.com>
2021-07-26 20:47:13 +0100

    Building DirectoryVideoLayoutItemViewHolder

--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/directoryListDisplayFragment/viewholders/DirectoryVideoLayoutItemViewHolder.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/directoryListDisplayFragment/viewholders/DirectoryVideoLayoutItemViewHolder.kt
@@ -4,13 +4,33 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.salesground.zipbolt.R
 import com.salesground.zipbolt.databinding.FolderVideoLayoutItemBinding
+import com.salesground.zipbolt.model.DataToTransfer
 
 class DirectoryVideoLayoutItemViewHolder(
     private val folderVideoLayoutItemBinding: FolderVideoLayoutItemBinding
 ) : RecyclerView.ViewHolder(folderVideoLayoutItemBinding.root) {
 
+
+    fun bindData(dataToTransfer: DataToTransfer) {
+        dataToTransfer as DataToTransfer.DeviceFile
+        folderVideoLayoutItemBinding.apply {
+            imageFile = dataToTransfer.file
+
+            Glide.with(root.context)
+                .load(dataToTransfer.file)
+                .into(folderVideoLayoutItemImageView)
+
+            folderVideoLayoutItemLayoutViewGroup.setOnClickListener {
+
+            }
+            folderVideoLayoutItemFolderSelectedCheckBox.setOnClickListener {
+
+            }
+        }
+    }
 
     companion object {
         fun createViewHolder(parent: ViewGroup): DirectoryVideoLayoutItemViewHolder {
