8a12de786d11f5a7172d79aee81e8002bc703f5b
Prosper Ekwerike <pekwerike@gmail.com>
2021-07-27 21:35:30 +0100

    Created ImageViewBindingAdapter class

--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/directoryListDisplayFragment/viewholders/DirectoryDocumentLayoutItemViewHolder.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/directoryListDisplayFragment/viewholders/DirectoryDocumentLayoutItemViewHolder.kt
@@ -1,4 +1,30 @@
 package com.salesground.zipbolt.ui.recyclerview.directoryListDisplayFragment.viewholders
 
-class DirectoryDocumentLayoutItemViewHolder {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.databinding.FolderDocumentLayoutItemBinding
+import com.salesground.zipbolt.model.DataToTransfer
+
+class DirectoryDocumentLayoutItemViewHolder(
+    private val folderDocumentLayoutItemBinding: FolderDocumentLayoutItemBinding
+) : RecyclerView.ViewHolder(folderDocumentLayoutItemBinding.root) {
+
+    fun bindData(dataToTransfer: DataToTransfer){
+
+    }
+
+    companion object {
+        fun createViewHolder(parent: ViewGroup): DirectoryDocumentLayoutItemViewHolder {
+            val layoutBinding = DataBindingUtil.inflate<FolderDocumentLayoutItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.folder_document_layout_item,
+                parent,
+                false
+            )
+            return DirectoryDocumentLayoutItemViewHolder(layoutBinding)
+        }
+    }
 }
