e727c34827cfd913cf878a17f2b8bc57e2141272
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-03 20:08:07 +0100

    Building sentDataFragment and receivedDataFragment UI

--- app/src/main/java/com/salesground/zipbolt/viewmodel/DataToTransferViewModel.kt
+++ app/src/main/java/com/salesground/zipbolt/viewmodel/DataToTransferViewModel.kt
@@ -1,11 +1,22 @@
 package com.salesground.zipbolt.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.salesground.zipbolt.model.DataToTransfer
 
 class DataToTransferViewModel : ViewModel() {
 
     private var collectionOfDataToTransfer: MutableList<DataToTransfer> = mutableListOf()
+
+    private val sentDataItemsNormalList: MutableList<DataToTransfer> = mutableListOf()
+    private val _sentDataItems = MutableLiveData<MutableList<DataToTransfer>>(mutableListOf())
+    val sentDataItems: LiveData<MutableList<DataToTransfer>>
+        get() = _sentDataItems
+
+    private val _receivedDataItems = MutableLiveData<MutableList<DataToTransfer>>(mutableListOf())
+    val receivedDataItems: LiveData<MutableList<DataToTransfer>>
+        get() = _receivedDataItems
 
     fun addDataToTransfer(dataToTransfer: DataToTransfer) {
         collectionOfDataToTransfer.add(dataToTransfer)
@@ -22,4 +33,13 @@ class DataToTransferViewModel : ViewModel() {
     fun getCollectionOfDataToTransfer(): MutableList<DataToTransfer> {
         return collectionOfDataToTransfer
     }
+
+    fun addCollectionOfDataToTransferToSentDataItems() {
+        sentDataItemsNormalList.addAll(collectionOfDataToTransfer.map {
+            it.transferStatus = DataToTransfer.TransferStatus.TRANSFER_ONGOING
+        } as MutableList<DataToTransfer>)
+
+        _sentDataItems.value = sentDataItemsNormalList
+    }
+
 }
