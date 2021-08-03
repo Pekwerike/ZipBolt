42a665504734754829e610542c002fd6517c22bb
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-03 22:16:17 +0100

    Created SentDataViewModel

--- app/src/main/java/com/salesground/zipbolt/viewmodel/DataToTransferViewModel.kt
+++ app/src/main/java/com/salesground/zipbolt/viewmodel/DataToTransferViewModel.kt
@@ -9,22 +9,13 @@ class DataToTransferViewModel : ViewModel() {
 
     private var collectionOfDataToTransfer: MutableList<DataToTransfer> = mutableListOf()
 
-    private val sentDataItemsNormalList: MutableList<DataToTransfer> = mutableListOf()
-    private val _sentDataItems = MutableLiveData<MutableList<DataToTransfer>>(mutableListOf())
-    val sentDataItems: LiveData<MutableList<DataToTransfer>>
-        get() = _sentDataItems
+
 
     private val _receivedDataItems = MutableLiveData<MutableList<DataToTransfer>>(mutableListOf())
     val receivedDataItems: LiveData<MutableList<DataToTransfer>>
         get() = _receivedDataItems
 
-    private val _updatedSentDataItemIndex = MutableLiveData(-1)
-    val updatedSentDataItemIndex: LiveData<Int>
-        get() = _updatedSentDataItemIndex
 
-    private val _canceledSentDataItemIndex = MutableLiveData(-1)
-    val canceledSentDataItemIndex: LiveData<Int>
-        get() = _updatedSentDataItemIndex
 
     fun addDataToTransfer(dataToTransfer: DataToTransfer) {
         collectionOfDataToTransfer.add(dataToTransfer)
@@ -42,31 +33,5 @@ class DataToTransferViewModel : ViewModel() {
         return collectionOfDataToTransfer
     }
 
-    fun dataTransferCompleted(dataToTransfer: DataToTransfer) {
-        sentDataItemsNormalList.find {
-            it.dataUri == dataToTransfer.dataUri
-        }?.let {
-            it.transferStatus = DataToTransfer.TransferStatus.TRANSFER_COMPLETE
-            _updatedSentDataItemIndex.value = sentDataItemsNormalList.indexOf(it)
-        }
-    }
-
-    fun addCollectionOfDataToTransferToSentDataItems() {
-        sentDataItemsNormalList.addAll(collectionOfDataToTransfer.map {
-            it.transferStatus = DataToTransfer.TransferStatus.TRANSFER_ONGOING
-        } as MutableList<DataToTransfer>)
-
-        _sentDataItems.value = sentDataItemsNormalList
-    }
-
-    fun cancelDataTransfer(dataToTransfer: DataToTransfer) {
-        sentDataItemsNormalList.find {
-            it.dataUri == dataToTransfer.dataUri
-        }?.let {
-            val removedIndex = sentDataItemsNormalList.indexOf(it)
-            sentDataItemsNormalList.remove(it)
-            _canceledSentDataItemIndex.value = removedIndex
-        }
-    }
 
 }
