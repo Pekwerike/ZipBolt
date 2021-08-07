0c74c325b16602f8869032f9e68a49f81112f110
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-07 21:46:36 +0100

    Building PlainFileMediaTransferProtocolTest

--- app/src/androidTest/java/com/salesground/zipbolt/communication/implementation/PlainFileMediaTransferProtocolTest.kt
+++ app/src/androidTest/java/com/salesground/zipbolt/communication/implementation/PlainFileMediaTransferProtocolTest.kt
@@ -1,5 +1,24 @@
 package com.salesground.zipbolt.communication.implementation
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.salesground.zipbolt.repository.ZipBoltSavedFilesRepository
 import org.junit.Assert.*
+import java.io.*
 
-class PlainFileMediaTransferProtocolTest
+class PlainFileMediaTransferProtocolTest {
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val gateWayFile = File(
+        context.getExternalFilesDir(null),
+        "gateWay.txt"
+    )
+    private val gateWayFileOutputStream =
+        DataOutputStream(BufferedOutputStream(FileOutputStream(gateWayFile)))
+    private val gateWayFileInputStream =
+        DataInputStream(BufferedInputStream(FileInputStream(gateWayFile)))
+
+    private val plainFileMediaTransferProtocol: PlainFileMediaTransferProtocol =
+        PlainFileMediaTransferProtocol(ZipBoltSavedFilesRepository())
+
+
+}
