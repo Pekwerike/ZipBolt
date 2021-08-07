d44a3015fa9320104244dbe1fd66cbbfb61fd0d3
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-07 20:07:09 +0100

    Building PlainFileMediaTransferProtocol

--- app/src/main/java/com/salesground/zipbolt/communication/implementation/PlainFileMediaTransferProtocol.kt
+++ app/src/main/java/com/salesground/zipbolt/communication/implementation/PlainFileMediaTransferProtocol.kt
@@ -10,11 +10,30 @@ import java.io.BufferedInputStream
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.FileInputStream
+import kotlin.math.min
 
 class PlainFileMediaTransferProtocol(savedFilesRepository: SavedFilesRepository) {
-
+    private var mTransferMetaData =
+        MediaTransferProtocol.MediaTransferProtocolMetaData.KEEP_RECEIVING
     private val receiveBuffer = ByteArray(DataTransferService.BUFFER_SIZE)
     private val transferBuffer = ByteArray(DataTransferService.BUFFER_SIZE)
+
+    fun cancelCurrentTransfer(transferMetaData: MediaTransferProtocol.MediaTransferProtocolMetaData) {
+        mTransferMetaData = transferMetaData
+    }
+
+    private fun resetTransferMetaData(
+        dataTransferListener: MediaTransferProtocol.DataTransferListener,
+        dataToTransfer: DataToTransfer
+    ) {
+        dataTransferListener.onTransfer(
+            dataToTransfer,
+            0f,
+            DataToTransfer.TransferStatus.TRANSFER_CANCELLED
+        )
+        // return the mTransferMetaData to keep receiving incase of reuse
+        mTransferMetaData = MediaTransferProtocol.MediaTransferProtocolMetaData.KEEP_RECEIVING
+    }
 
     fun transferFile(
         dataToTransfer: DataToTransfer,
@@ -38,10 +57,41 @@ class PlainFileMediaTransferProtocol(savedFilesRepository: SavedFilesRepository)
             0f,
             DataToTransfer.TransferStatus.TRANSFER_STARTED
         )
+        var lengthRead: Int
         while (unreadDataSize > 0) {
+            lengthRead = min(unreadDataSize, transferBuffer.size.toLong()).toInt()
+            fileDataInputStream.readFully(
+                transferBuffer, 0, lengthRead
+            )
+            //*** write the current transfer state to receiver
+            dataOutputStream.writeInt(mTransferMetaData.value)
 
+            if (mTransferMetaData == MediaTransferProtocol.MediaTransferProtocolMetaData.CANCEL_ACTIVE_RECEIVE) {
+                resetTransferMetaData(dataTransferListener, dataToTransfer)
+                return
+            } else if (mTransferMetaData == MediaTransferProtocol.MediaTransferProtocolMetaData.KEEP_RECEIVING_BUT_CANCEL_ACTIVE_TRANSFER) {
+                mTransferMetaData =
+                    MediaTransferProtocol.MediaTransferProtocolMetaData.KEEP_RECEIVING
+            }
+
+            dataOutputStream.write(
+                transferBuffer,
+                0, lengthRead
+            )
+
+            unreadDataSize -= lengthRead
+
+            dataTransferListener.onTransfer(
+                dataToTransfer,
+                ((dataToTransfer.dataSize - unreadDataSize) / dataToTransfer.dataSize.toFloat()) * 100f,
+                DataToTransfer.TransferStatus.TRANSFER_ONGOING
+            )
         }
-
-
+        fileDataInputStream.close()
+        dataTransferListener.onTransfer(
+            dataToTransfer,
+            100f,
+            DataToTransfer.TransferStatus.TRANSFER_COMPLETE
+        )
     }
 }
