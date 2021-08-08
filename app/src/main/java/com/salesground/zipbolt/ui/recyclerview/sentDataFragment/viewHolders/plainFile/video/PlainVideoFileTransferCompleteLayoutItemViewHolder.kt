f9a14d6fba2eab3cbd6d22f004d746d77be35c74
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-08 06:40:17 +0100

    Building PlainVideoFileTransferCompleteLayoutItemViewHolder and PlainVideoFileTransferWaitingLayoutItemViewHolder

---
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/sentDataFragment/viewHolders/plainFile/video/PlainVideoFileTransferCompleteLayoutItemViewHolder.kt
@@ -0,0 +1,33 @@
+package com.salesground.zipbolt.ui.recyclerview.sentDataFragment.viewHolders.plainFile.video
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.databinding.PlainVideoFileTransferLayoutItemBinding
+import com.salesground.zipbolt.model.DataToTransfer
+
+class PlainVideoFileTransferCompleteLayoutItemViewHolder (
+    private val plainVideoFileTransferLayoutItemBinding: PlainVideoFileTransferLayoutItemBinding
+) : RecyclerView.ViewHolder(plainVideoFileTransferLayoutItemBinding.root) {
+
+    fun bindData(dataToTransfer: DataToTransfer) {
+        plainVideoFileTransferLayoutItemBinding.run {
+            this.document = dataToTransfer as DataToTransfer.DeviceFile
+            plainFileTransferLayoutItemShimmer
+        }
+    }
+
+    companion object {
+        fun createViewHolder(parent: ViewGroup): PlainVideoFileTransferCompleteLayoutItemViewHolder {
+            val layoutBinding = DataBindingUtil.inflate<PlainVideoFileTransferLayoutItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.plain_video_file_transfer_layout_item,
+                parent,
+                false
+            )
+            return PlainVideoFileTransferCompleteLayoutItemViewHolder(layoutBinding)
+        }
+    }
+}
