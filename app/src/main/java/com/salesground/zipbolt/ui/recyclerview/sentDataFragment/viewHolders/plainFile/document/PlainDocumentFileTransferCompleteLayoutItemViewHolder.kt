cc9e7d4407e9e5c2fc984c1d997d93c3503ba86e
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-08 09:24:29 +0100

    Created PlainDocumentFileTransferCompleteLayoutItemViewHolder

---
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/sentDataFragment/viewHolders/plainFile/document/PlainDocumentFileTransferCompleteLayoutItemViewHolder.kt
@@ -0,0 +1,35 @@
+package com.salesground.zipbolt.ui.recyclerview.sentDataFragment.viewHolders.plainFile.document
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.salesground.zipbolt.R
+import com.salesground.zipbolt.databinding.PlainDocumentFileTransferLayoutItemBinding
+import com.salesground.zipbolt.model.DataToTransfer
+
+class PlainDocumentFileTransferCompleteLayoutItemViewHolder(
+    private val plainDocumentFileTransferLayoutItemBinding: PlainDocumentFileTransferLayoutItemBinding
+) : RecyclerView.ViewHolder(plainDocumentFileTransferLayoutItemBinding.root) {
+
+    fun bindData(dataToTransfer: DataToTransfer){
+        plainDocumentFileTransferLayoutItemBinding.run {
+            document = dataToTransfer as DataToTransfer.DeviceFile
+            plainDocumentFileTransferLayoutItemShimmer.run {
+                stopShimmer()
+                hideShimmer()
+            }
+        }
+    }
+    companion object {
+        fun createViewHolder(parent: ViewGroup): PlainDocumentFileTransferCompleteLayoutItemViewHolder {
+            val layoutBinding = DataBindingUtil.inflate<PlainDocumentFileTransferLayoutItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.plain_document_file_transfer_layout_item,
+                parent,
+                false
+            )
+            return PlainDocumentFileTransferCompleteLayoutItemViewHolder(layoutBinding)
+        }
+    }
+}
