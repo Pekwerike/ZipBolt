c110303e32c1db374feb8c15b4f742dd7d288944
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-08 13:01:11 +0100

    Refactored AudioTransferWaitingLayoutItemViewHolder and AudioTransferCompleteLayoutItemViewHolder

--- app/src/main/java/com/salesground/zipbolt/ui/recyclerview/sentDataFragment/viewHolders/audio/AudioTransferWaitingLayoutItemViewHolder.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/recyclerview/sentDataFragment/viewHolders/audio/AudioTransferWaitingLayoutItemViewHolder.kt
@@ -6,32 +6,30 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.salesground.zipbolt.R
-import com.salesground.zipbolt.databinding.VideoTransferLayoutItemBinding
+import com.salesground.zipbolt.databinding.AudioTransferLayoutItemBinding
 import com.salesground.zipbolt.model.DataToTransfer
 
 class AudioTransferWaitingLayoutItemViewHolder(
-    private val audioTransferLayoutItemBinding: VideoTransferLayoutItemBinding
+    private val audioTransferLayoutItemBinding: AudioTransferLayoutItemBinding
 ) : RecyclerView.ViewHolder(audioTransferLayoutItemBinding.root) {
 
     fun bindData(dataToTransfer: DataToTransfer) {
         dataToTransfer as DataToTransfer.DeviceAudio
         audioTransferLayoutItemBinding.run {
-            videoSize = dataToTransfer.audioSize
-            videoName = dataToTransfer.audioDisplayName
-            videoDuration = dataToTransfer.audioDuration
+            this.dataToTransfer = dataToTransfer
 
-            Glide.with(videoTransferLayoutItemVideoPreviewImageView)
+            Glide.with(audioTransferLayoutItemVideoPreviewImageView)
                 .load(dataToTransfer.dataUri)
                 .error(R.drawable.ic_baseline_music_note_24)
-                .into(videoTransferLayoutItemVideoPreviewImageView)
+                .into(audioTransferLayoutItemVideoPreviewImageView)
         }
     }
 
     companion object {
         fun createViewHolder(parent: ViewGroup): AudioTransferWaitingLayoutItemViewHolder {
-            val layoutBinding = DataBindingUtil.inflate<VideoTransferLayoutItemBinding>(
+            val layoutBinding = DataBindingUtil.inflate<AudioTransferLayoutItemBinding>(
                 LayoutInflater.from(parent.context),
-                R.layout.video_transfer_layout_item,
+                R.layout.audio_transfer_layout_item,
                 parent,
                 false
             )
