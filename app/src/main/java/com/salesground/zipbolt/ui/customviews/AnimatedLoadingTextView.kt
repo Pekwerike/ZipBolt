32b1bad58e205c9805db85774809473532b860e1
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-09 10:58:41 +0100

    Bug fix and code refactor

--- app/src/main/java/com/salesground/zipbolt/ui/customviews/AnimatedLoadingTextView.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/customviews/AnimatedLoadingTextView.kt
@@ -1,8 +1,7 @@
 package com.salesground.zipbolt.ui.customviews
 
 import android.animation.ValueAnimator
-import android.animation.ValueAnimator.INFINITE
-import android.animation.ValueAnimator.RESTART
+import android.animation.ValueAnimator.*
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
@@ -12,11 +11,13 @@ import com.google.android.material.textview.MaterialTextView
 class AnimatedLoadingTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : MaterialTextView(context, attrs) {
-    private val stringBuilder = StringBuilder()
     private var initialTextSize: Int = 0
+    private val initialText: String = text.toString()
+    private val threeDots: String = "$initialText..."
+    private val twoDots: String = "$initialText.."
+    private val oneDot: String = "$initialText."
 
     init {
-        stringBuilder.append(text.toString())
         animateText()
     }
 
@@ -26,23 +27,28 @@ class AnimatedLoadingTextView @JvmOverloads constructor(
 
     fun animateText() {
         initialTextSize = text.length
-        stringBuilder.append(text.toString())
-
-        val valueAnimator: ValueAnimator = ValueAnimator.ofInt(0, 3).apply {
-            repeatMode = RESTART
+        val valueAnimator: ValueAnimator = ofInt(0, 4).apply {
+            repeatMode = REVERSE
             repeatCount = INFINITE
-            duration = 1000
+            duration = 2500
             start()
         }
         valueAnimator.addUpdateListener {
             text = when (it.animatedValue) {
                 0 -> {
-                    stringBuilder.substring(initialTextSize, text.length)
-                    stringBuilder
+                    initialText
+                }
+                1 -> {
+                    oneDot
+                }
+                2 -> {
+                    twoDots
+                }
+                3 -> {
+                    threeDots
                 }
                 else -> {
-                    stringBuilder.append(".")
-                    stringBuilder
+                    threeDots
                 }
             }
         }
