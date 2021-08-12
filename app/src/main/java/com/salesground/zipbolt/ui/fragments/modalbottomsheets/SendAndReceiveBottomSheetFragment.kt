567fb2dbeac33b8c99edcbf08d325e3e1e85193f
Prosper Ekwerike <pekwerike@gmail.com>
2021-08-12 23:49:40 +0100

    Built fragment_send_and_receive_bottom_sheet

--- app/src/main/java/com/salesground/zipbolt/ui/fragments/modalbottomsheets/SendAndReceiveBottomSheetFragment.kt
+++ app/src/main/java/com/salesground/zipbolt/ui/fragments/modalbottomsheets/SendAndReceiveBottomSheetFragment.kt
@@ -12,11 +12,6 @@ import com.salesground.zipbolt.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SendAndReceiveBottomSheetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SendAndReceiveBottomSheetFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
